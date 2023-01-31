b9b7322a068a098bd8e6bbb583f58e10654e5dd7
Jimmi Dyson <jimmidyson@gmail.com>
2023-01-31 18:29:17 +0000

    build: Initial repo setup

---
+++ make/make.mk
@@ -0,0 +1,5 @@
+# Copyright 2023 D2iQ, Inc. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+MAKEFLAGS += --no-builtin-rules
+MAKEFLAGS += --no-builtin-variables
---
+++ make/upx.mk
@@ -0,0 +1,22 @@
+# Copyright 2023 D2iQ, Inc. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+SKIP_UPX := true
+
+.PHONY: upx
+upx: UPX_REAL_TARGET := $(addsuffix $(if $(filter $(GOOS),windows),.exe),$(basename $(UPX_TARGET)))
+ifneq ($(SKIP_UPX),true)
+ifeq ($(GOOS)/$(GOARCH),windows/arm64)
+upx: ; $(info $(M) skipping packing $(UPX_REAL_TARGET) - $(GOOS)/$(GOARCH) is not yet supported by upx)
+# TODO Remove once upx 4.0.2 is released
+else ifeq ($(GOOS)/$(GOARCH),darwin/arm64)
+upx: ; $(info $(M) skipping packing $(UPX_REAL_TARGET) - $(GOOS)/$(GOARCH) has a bug in packing - https://github.com/upx/upx/issues/628 - should be fixed in 4.0.2)
+else
+upx: install-tool.upx
+upx: ## Pack executable using upx
+upx: ; $(info $(M) packing $(UPX_REAL_TARGET))
+	(upx -l $(UPX_REAL_TARGET) &>/dev/null && echo $(UPX_REAL_TARGET) is already packed) || upx -9 --lzma $(UPX_REAL_TARGET)
+# Double check file is successfully compressed - seen errors with macos binaries
+	upx -t $(UPX_REAL_TARGET) &>/dev/null || (echo $(UPX_REAL_TARGET) is broken after upx compression && exit 1)
+endif
+endif
