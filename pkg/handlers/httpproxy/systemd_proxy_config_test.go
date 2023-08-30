85a68e06ece929de7abbb902384070ba83ed248e
Jimmi Dyson <jimmidyson@gmail.com>
2023-08-30 13:24:18 +0100

    refactor: Adopt simpler proxy generator funcs
    Small tweak that satisfies my desire for side-effect free functions
    and I have also updated tests to have slightly better checks.

--- pkg/handlers/httpproxy/systemd_proxy_config_test.go
+++ pkg/handlers/httpproxy/systemd_proxy_config_test.go
@@ -10,107 +10,98 @@ import (
 	bootstrapv1 "sigs.k8s.io/cluster-api/bootstrap/kubeadm/api/v1beta1"
 )
 
-func newTestGenerator() *systemdConfigGenerator {
-	return &systemdConfigGenerator{
-		template: templates.Lookup("systemd.conf.tmpl"),
+func TestGenerateSystemdFiles(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name             string
+		vars             HTTPProxyVariables
+		expectedContents string
+	}{{
+		name: "no proxy configuration",
+	}, {
+		name: "all vars set",
+		vars: HTTPProxyVariables{
+			HTTP:  "http://example.com",
+			HTTPS: "https://example.com",
+			NO: []string{
+				"https://no-proxy.example.com",
+			},
+		},
+		expectedContents: `[Service]
+Environment="HTTP_PROXY=http://example.com"
+Environment="http_proxy=http://example.com"
+Environment="HTTPS_PROXY=https://example.com"
+Environment="https_proxy=https://example.com"
+Environment="NO_PROXY=https://no-proxy.example.com"
+Environment="no_proxy=https://no-proxy.example.com"
+`,
+	}, {
+		name: "http only",
+		vars: HTTPProxyVariables{
+			HTTP: "http://example.com",
+		},
+		expectedContents: `[Service]
+Environment="HTTP_PROXY=http://example.com"
+Environment="http_proxy=http://example.com"
+`,
+	}, {
+		name: "https only",
+		vars: HTTPProxyVariables{
+			HTTPS: "https://example.com",
+		},
+		expectedContents: `[Service]
+Environment="HTTPS_PROXY=https://example.com"
+Environment="https_proxy=https://example.com"
+`,
+	}, {
+		name: "no proxy only",
+		vars: HTTPProxyVariables{
+			NO: []string{
+				"https://no-proxy.example.com",
+			},
+		},
+		expectedContents: `[Service]
+Environment="NO_PROXY=https://no-proxy.example.com"
+Environment="no_proxy=https://no-proxy.example.com"
+`,
+	}, {
+		name: "multiple no proxy only",
+		vars: HTTPProxyVariables{
+			NO: []string{
+				"https://no-proxy.example.com",
+				"https://no-proxy-1.example.com",
+			},
+		},
+		expectedContents: `[Service]
+Environment="NO_PROXY=https://no-proxy.example.com,https://no-proxy-1.example.com"
+Environment="no_proxy=https://no-proxy.example.com,https://no-proxy-1.example.com"
+`,
+	}}
+
+	for idx := range tests {
+		tt := tests[idx]
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			g := NewWithT(t)
+
+			var expectedFiles []bootstrapv1.File
+			if tt.expectedContents != "" {
+				expectedFiles = []bootstrapv1.File{{
+					Path:        systemdUnitPaths[0],
+					Content:     tt.expectedContents,
+					Permissions: "0640",
+					Owner:       "root",
+				}, {
+					Path:        systemdUnitPaths[1],
+					Content:     tt.expectedContents,
+					Permissions: "0640",
+					Owner:       "root",
+				}}
+			}
+
+			g.Expect(generateSystemdFiles(tt.vars)).Should(Equal(expectedFiles))
+		})
 	}
-}
-
-func TestGenerate(t *testing.T) {
-	g := NewWithT(t)
-
-	content, err := newTestGenerator().Generate(HTTPProxyVariables{
-		HTTP:  "http://example.com",
-		HTTPS: "https://example.com",
-		NO: []string{
-			"https://no-proxy.example.com",
-		},
-	})
-	g.Expect(err).NotTo(HaveOccurred())
-	g.Expect(content).To(And(
-		ContainSubstring(`Environment="HTTP_PROXY=http://example.com"`),
-		ContainSubstring(`Environment="http_proxy=http://example.com"`),
-		ContainSubstring(`Environment="HTTPS_PROXY=https://example.com"`),
-		ContainSubstring(`Environment="https_proxy=https://example.com"`),
-		ContainSubstring(`Environment="NO_PROXY=https://no-proxy.example.com"`),
-		ContainSubstring(`Environment="no_proxy=https://no-proxy.example.com"`),
-	))
-}
-
-func TestGenerate_OnlyHTTP(t *testing.T) {
-	g := NewWithT(t)
-
-	content, err := newTestGenerator().Generate(HTTPProxyVariables{
-		HTTP: "http://example.com",
-	})
-	g.Expect(err).NotTo(HaveOccurred())
-	g.Expect(content).To(And(
-		ContainSubstring(`Environment="HTTP_PROXY=http://example.com"`),
-		ContainSubstring(`Environment="http_proxy=http://example.com"`),
-		Not(ContainSubstring(`Environment="HTTPS_PROXY=`)),
-		Not(ContainSubstring(`Environment="https_proxy=`)),
-		Not(ContainSubstring(`Environment="NO_PROXY=`)),
-		Not(ContainSubstring(`Environment="no_proxy=`)),
-	))
-}
-
-func TestGenerate_OnlyHTTPS(t *testing.T) {
-	g := NewWithT(t)
-
-	content, err := newTestGenerator().Generate(HTTPProxyVariables{
-		HTTPS: "https://example.com",
-	})
-	g.Expect(err).NotTo(HaveOccurred())
-	g.Expect(content).To(And(
-		Not(ContainSubstring(`Environment="HTTP_PROXY=http://example.com"`)),
-		Not(ContainSubstring(`Environment="http_proxy=http://example.com"`)),
-		ContainSubstring(`Environment="HTTPS_PROXY=https://example.com"`),
-		ContainSubstring(`Environment="https_proxy=https://example.com"`),
-		Not(ContainSubstring(`Environment="NO_PROXY=https://no-proxy.example.com"`)),
-		Not(ContainSubstring(`Environment="no_proxy=https://no-proxy.example.com"`)),
-	))
-}
-
-func TestGenerate_OnlyNoProxy(t *testing.T) {
-	g := NewWithT(t)
-
-	content, err := newTestGenerator().Generate(HTTPProxyVariables{
-		NO: []string{"https://no-proxy.example.com"},
-	})
-	g.Expect(err).NotTo(HaveOccurred())
-	g.Expect(content).To(And(
-		Not(ContainSubstring(`Environment="HTTP_PROXY="`)),
-		Not(ContainSubstring(`Environment="http_proxy="`)),
-		Not(ContainSubstring(`Environment="HTTPS_PROXY="`)),
-		Not(ContainSubstring(`Environment="https_proxy=`)),
-		ContainSubstring(`Environment="NO_PROXY=https://no-proxy.example.com"`),
-		ContainSubstring(`Environment="no_proxy=https://no-proxy.example.com"`),
-	))
-}
-
-func TestGenerate_NoProxyMultipleURLs(t *testing.T) {
-	g := NewWithT(t)
-
-	content, err := newTestGenerator().Generate(HTTPProxyVariables{
-		NO: []string{
-			"https://no-proxy.example.com",
-			"https://no-proxy-1.example.com",
-		},
-	})
-	g.Expect(err).NotTo(HaveOccurred())
-	g.Expect(content).To(And(
-		ContainSubstring(
-			`Environment="NO_PROXY=https://no-proxy.example.com,https://no-proxy-1.example.com"`,
-		),
-		ContainSubstring(
-			`Environment="no_proxy=https://no-proxy.example.com,https://no-proxy-1.example.com"`,
-		),
-	))
-}
-
-func TestAddSystemdFiles(t *testing.T) {
-	g := NewWithT(t)
-
-	dst := []bootstrapv1.File{}
-	g.Expect(newTestGenerator().AddSystemdFiles(HTTPProxyVariables{}, dst)).To(HaveLen(2))
 }
