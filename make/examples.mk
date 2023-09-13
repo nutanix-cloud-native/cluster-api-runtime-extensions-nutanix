91ece9069f2b955d273a780be012e63f2ae1d6a3
Jimmi Dyson <jimmidyson@gmail.com>
2023-09-13 14:39:03 +0100

    build: Use kustomize instead of gojq directly

--- make/examples.mk
+++ make/examples.mk
@@ -1,7 +1,6 @@
 # Copyright 2023 D2iQ, Inc. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-
 KUBERNETES_VERSION ?= v1.27.5
 
 # gojq mutations:
@@ -11,30 +10,10 @@ KUBERNETES_VERSION ?= v1.27.5
 # - Cluster: 		Add an empty clusterConfig variable
 .PHONY: examples.sync
 examples.sync: ## Syncs the examples by fetching upstream examples using clusterclt and applying gojq mutations
-examples.sync: export KUBECONFIG := $(KIND_KUBECONFIG)
-examples.sync: kind.create clusterctl.init
 	mkdir -p examples/capi-quickstart
 	# Sync ClusterClass and all Templates
-	clusterctl generate cluster capi-quickstart \
-      --flavor development \
-      --kubernetes-version $(KUBERNETES_VERSION) \
-      --control-plane-machine-count=1 \
-      --worker-machine-count=1 | \
-    gojq --yaml-input --yaml-output \
-      '. | (select(.kind=="ClusterClass").spec.patches|= .+ [{"name": "cluster-config", "external": {"generateExtension": "clusterconfigpatch.capi-runtime-extensions", "discoverVariablesExtension": "clusterconfigvars.capi-runtime-extensions"}}])' | \
-    gojq --yaml-input --yaml-output \
-      '. | select(.kind != "Cluster")' | \
-    gojq --yaml-input --yaml-output '.' > examples/capi-quickstart/cluster-class.yaml
+	kustomize build ./hack/examples | \
+	  gojq --yaml-input --yaml-output '. | select(.kind != "Cluster")' > examples/capi-quickstart/capd-cluster-class.yaml
 	# Sync Cluster
-	clusterctl generate cluster capi-quickstart \
-      --flavor development \
-      --kubernetes-version $(KUBERNETES_VERSION) \
-      --control-plane-machine-count=1 \
-      --worker-machine-count=1 | \
-    gojq --yaml-input --yaml-output \
-      '. | (select(.kind=="Cluster").metadata.labels["capiext.labs.d2iq.io/cni"]|="calico")' | \
-    gojq --yaml-input --yaml-output \
-    	'. | (select(.kind=="Cluster").spec.topology.variables|= .+ [{"name": "clusterConfig", "value": {}}])' | \
-    gojq --yaml-input --yaml-output \
-      '. | select(.kind == "Cluster")' | \
-    gojq --yaml-input --yaml-output '.' > examples/capi-quickstart/cluster.yaml
+	kustomize build ./hack/examples | \
+	  gojq --yaml-input --yaml-output '. | select(.kind == "Cluster")' > examples/capi-quickstart/capd-cluster.yaml
