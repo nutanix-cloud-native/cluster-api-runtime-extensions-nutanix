2628ca34075e96972143ed906708c91b99cdedb1
Jimmi Dyson <jimmidyson@gmail.com>
2023-09-14 15:44:24 +0100

    fixup! build: Use kubernetes version in generated examples

--- hack/examples/sync.sh
+++ hack/examples/sync.sh
@@ -9,12 +9,15 @@ IFS=$'\n\t'
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly SCRIPT_DIR
 
-CAPD_KUSTOMIZATION_FILE="${SCRIPT_DIR}/bases/capd/kustomization.yaml"
+readonly EXAMPLES_KUSTOMIZATION_FILE="${SCRIPT_DIR}/kustomization.yaml"
+readonly CAPD_KUSTOMIZATION_FILE="${SCRIPT_DIR}/bases/capd/kustomization.yaml"
 
+trap 'rm -rf ${CAPD_KUSTOMIZATION_FILE} ${EXAMPLES_KUSTOMIZATION_FILE}' EXIT
 # download the quick-start files that match the clusterctl version
-trap 'rm -rf ${CAPD_KUSTOMIZATION_FILE}' EXIT
 CLUSTERCTL_VERSION=$(clusterctl version -o short 2>/dev/null) envsubst \
-  <"${SCRIPT_DIR}/bases/capd/kustomization.yaml.tmpl" >"${CAPD_KUSTOMIZATION_FILE}"
+  <"${CAPD_KUSTOMIZATION_FILE}.tmpl" >"${CAPD_KUSTOMIZATION_FILE}"
+# replace the kubernetes version
+envsubst <"${EXAMPLES_KUSTOMIZATION_FILE}.tmpl" >"${EXAMPLES_KUSTOMIZATION_FILE}"
 
 mkdir -p examples/capi-quick-start
 # Sync ClusterClass and all Templates
--- make/examples.mk
+++ make/examples.mk
@@ -1,6 +1,9 @@
 # Copyright 2023 D2iQ, Inc. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+KUBERNETES_VERSION := v1.27.5
+
 .PHONY: examples.sync
+examples.sync: export KUBERNETES_VERSION := $(KUBERNETES_VERSION)
 examples.sync: ## Syncs the examples by fetching upstream examples and applying kustomize patches
 	hack/examples/sync.sh
