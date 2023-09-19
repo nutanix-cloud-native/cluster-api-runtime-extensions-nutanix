33358960476d502edbc5d0bf7425cba5ca5a1655
Jimmi Dyson <jimmidyson@gmail.com>
2023-09-19 18:01:23 +0100

    build: Remove currently unused flux

--- make/flux.mk
+++
@@ -1,6 +0,0 @@
-# Copyright 2023 D2iQ, Inc. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-.PHONY: flux.install
-flux.install:
-	flux install --components=source-controller,helm-controller
