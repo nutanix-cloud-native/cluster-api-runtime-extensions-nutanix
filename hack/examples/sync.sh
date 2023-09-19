56d967d245061a5d5b643d9d8bf26020c1dc752d
Jimmi Dyson <jimmidyson@gmail.com>
2023-09-19 17:27:50 +0100

    build: Add AWS clusterclass example

--- hack/examples/sync.sh
+++ hack/examples/sync.sh
@@ -10,19 +10,22 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly SCRIPT_DIR
 
 readonly EXAMPLES_KUSTOMIZATION_FILE="${SCRIPT_DIR}/kustomization.yaml"
-readonly CAPD_KUSTOMIZATION_FILE="${SCRIPT_DIR}/bases/capd/kustomization.yaml"
+readonly DOCKER_KUSTOMIZATION_FILE="${SCRIPT_DIR}/bases/docker/kustomization.yaml"
+readonly AWS_KUSTOMIZATION_FILE="${SCRIPT_DIR}/bases/aws/kustomization.yaml"
 
-trap 'rm -rf ${CAPD_KUSTOMIZATION_FILE} ${EXAMPLES_KUSTOMIZATION_FILE}' EXIT
+trap 'find "${SCRIPT_DIR}" -name kustomization.yaml -delete' EXIT
 # download the quick-start files that match the clusterctl version
-CLUSTERCTL_VERSION=$(clusterctl version -o short 2>/dev/null) envsubst \
-  <"${CAPD_KUSTOMIZATION_FILE}.tmpl" >"${CAPD_KUSTOMIZATION_FILE}"
+envsubst <"${DOCKER_KUSTOMIZATION_FILE}.tmpl" >"${DOCKER_KUSTOMIZATION_FILE}"
+envsubst <"${AWS_KUSTOMIZATION_FILE}.tmpl" >"${AWS_KUSTOMIZATION_FILE}"
+
 # replace the kubernetes version
 envsubst -no-unset <"${EXAMPLES_KUSTOMIZATION_FILE}.tmpl" >"${EXAMPLES_KUSTOMIZATION_FILE}"
 
 mkdir -p examples/capi-quick-start
-# Sync ClusterClass and all Templates
+# Sync ClusterClasses (including Templates) and Clusters to separate files
 kustomize build ./hack/examples |
-  gojq --yaml-input --yaml-output '. | select(.kind != "Cluster")' >examples/capi-quick-start/capd-cluster-class.yaml
-# Sync Cluster
-kustomize build ./hack/examples |
-  gojq --yaml-input --yaml-output '. | select(.kind == "Cluster")' >examples/capi-quick-start/capd-cluster.yaml
+  tee >(gojq --yaml-input --yaml-output '. | select(.metadata.labels["cluster.x-k8s.io/provider"] == "docker" and .kind != "Cluster")' >examples/capi-quick-start/docker-cluster-class.yaml) \
+    >(gojq --yaml-input --yaml-output '. | select(.metadata.labels["cluster.x-k8s.io/provider"] == "docker" and .kind == "Cluster")' >examples/capi-quick-start/docker-cluster.yaml) \
+    >(gojq --yaml-input --yaml-output '. | select(.metadata.labels["cluster.x-k8s.io/provider"] == "aws" and .kind != "Cluster")' >examples/capi-quick-start/aws-cluster-class.yaml) \
+    >(gojq --yaml-input --yaml-output '. | select(.metadata.labels["cluster.x-k8s.io/provider"] == "aws" and .kind == "Cluster")' >examples/capi-quick-start/aws-cluster.yaml) \
+    >/dev/null
--- make/examples.mk
+++ make/examples.mk
@@ -1,9 +1,11 @@
 # Copyright 2023 D2iQ, Inc. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-KUBERNETES_VERSION := v1.27.5
+export KUBERNETES_VERSION := v1.27.5
+
+export CLUSTERCTL_VERSION := $(shell clusterctl version -o short 2>/dev/null)
+export CAPA_VERSION := v2.2.2
 
 .PHONY: examples.sync
-examples.sync: export KUBERNETES_VERSION := $(KUBERNETES_VERSION)
 examples.sync: ## Syncs the examples by fetching upstream examples and applying kustomize patches
 	hack/examples/sync.sh
