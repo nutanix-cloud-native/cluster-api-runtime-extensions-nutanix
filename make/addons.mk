a37a9b30d6e0e65279cf4692eed3a070a2afead0
faiq <faiqrazarizvi@gmail.com>
2023-09-19 23:15:25 -0700

    ci: starts process for automated nfd updates

--- make/addons.mk
+++ make/addons.mk
@@ -2,7 +2,10 @@
 # SPDX-License-Identifier: Apache-2.0
 
 export CALICO_VERSION := v3.26.1
+export NODE_FEATURE_VERSION := 0.11.1
 
 .PHONY: update-addon.calico
 update-addon.calico: ; $(info $(M) updating calico manifests)
 	./hack/addons/update-calico-manifests.sh
+update-addon.nfd: ; $(info $(M) updating node feature discovery manifests)
+	./hack/addons/update-node-feature-discovery-manifests.sh
