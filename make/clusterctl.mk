e3cc7d387373e29ef01e6acbffc8ea66a1cdd202
Dimitri Koshkin <dimitri.koshkin@gmail.com>
2023-09-20 12:43:10 -0700

    Revert "build: read AWS envs and pass to clusterctl"
    This reverts commit 3dfb9bd444d03e75bf29af82da985cd632ed7d48.

--- make/clusterctl.mk
+++ make/clusterctl.mk
@@ -1,22 +1,13 @@
 # Copyright 2023 D2iQ, Inc. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-define AWS_CREDENTIALS
-[default]
-aws_access_key_id = $(AWS_ACCESS_KEY_ID)
-aws_secret_access_key = $(AWS_SECRET_ACCESS_KEY)
-aws_session_token = $(AWS_SESSION_TOKEN)
-endef
-
-AWS_B64ENCODED_CREDENTIALS ?= $(shell echo "$$AWS_CREDENTIALS" | base64 -w0)
-
 .PHONY: clusterctl.init
 clusterctl.init:
 	env CLUSTER_TOPOLOGY=true \
 			EXP_RUNTIME_SDK=true \
 			EXP_CLUSTER_RESOURCE_SET=true \
 			EXP_MACHINE_POOL=true \
-			AWS_B64ENCODED_CREDENTIALS=$(AWS_B64ENCODED_CREDENTIALS) \
+			AWS_B64ENCODED_CREDENTIALS= \
 			clusterctl init \
 		--kubeconfig=$(KIND_KUBECONFIG) \
 		--infrastructure docker,aws \
