5d86cd503e3121e8889aeffcdb08fd825d208944
Jimmi Dyson <jimmidyson@gmail.com>
2023-09-28 09:27:27 +0100

    refactor: Move meta handlers to provider packages

---
+++ pkg/handlers/aws/mutation/handlers.go
@@ -0,0 +1,30 @@
+// Copyright 2023 D2iQ, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package mutation
+
+import (
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+
+	"github.com/d2iq-labs/capi-runtime-extensions/common/pkg/capi/apis"
+	"github.com/d2iq-labs/capi-runtime-extensions/common/pkg/capi/clustertopology/handlers"
+	"github.com/d2iq-labs/capi-runtime-extensions/common/pkg/capi/clustertopology/handlers/mutation"
+	"github.com/d2iq-labs/capi-runtime-extensions/pkg/handlers/aws/mutation/region"
+	genericmutation "github.com/d2iq-labs/capi-runtime-extensions/pkg/handlers/generic/mutation"
+)
+
+// MetaPatchHandler returns a meta patch handler for mutating CAPD clusters.
+func MetaPatchHandler(mgr manager.Manager) handlers.Named {
+	patchHandlers := append(
+		[]mutation.MetaMutater{
+			region.NewMetaPatch(),
+		},
+		genericmutation.MetaMutaters(mgr)...,
+	)
+
+	return mutation.NewMetaGeneratePatchesHandler(
+		"awsClusterConfigPatch",
+		apis.CAPADecoder(),
+		patchHandlers...,
+	)
+}
---
+++ pkg/handlers/generic/mutation/handlers.go
@@ -0,0 +1,26 @@
+// Copyright 2023 D2iQ, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package mutation
+
+import (
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+
+	"github.com/d2iq-labs/capi-runtime-extensions/common/pkg/capi/clustertopology/handlers/mutation"
+	"github.com/d2iq-labs/capi-runtime-extensions/pkg/handlers/generic/mutation/auditpolicy"
+	"github.com/d2iq-labs/capi-runtime-extensions/pkg/handlers/generic/mutation/etcd"
+	"github.com/d2iq-labs/capi-runtime-extensions/pkg/handlers/generic/mutation/extraapiservercertsans"
+	"github.com/d2iq-labs/capi-runtime-extensions/pkg/handlers/generic/mutation/httpproxy"
+	"github.com/d2iq-labs/capi-runtime-extensions/pkg/handlers/generic/mutation/kubernetesimagerepository"
+)
+
+// MetaMutaters returns all generic patch handlers.
+func MetaMutaters(mgr manager.Manager) []mutation.MetaMutater {
+	return []mutation.MetaMutater{
+		auditpolicy.NewPatch(),
+		etcd.NewMetaPatch(),
+		extraapiservercertsans.NewMetaPatch(),
+		httpproxy.NewMetaPatch(mgr.GetClient()),
+		kubernetesimagerepository.NewMetaPatch(),
+	}
+}
