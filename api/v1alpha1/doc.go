e1880d5dab12c11701c8a9ec5f680cb1ce11a3e0
Jimmi Dyson <jimmidyson@gmail.com>
2023-09-28 15:51:57 +0100

    build: Fix up RBAC role generation from annotations

--- api/v1alpha1/doc.go
+++ api/v1alpha1/doc.go
@@ -4,8 +4,4 @@
 // Package v1alpha1 contains API Schema definitions for the CAPI extensions v1alpha1 API group
 // +kubebuilder:object:generate=true
 // +groupName=capiext.labs.d2iq.io
-//
-//go:generate -command CTRLGEN controller-gen  paths="./..."
-//go:generate CTRLGEN rbac:headerFile="../../hack/license-header.yaml.txt",roleName=capi-runtime-extensions-manager-role output:rbac:artifacts:config=../../charts/capi-runtime-extensions/templates
-//go:generate CTRLGEN object:headerFile="../../hack/license-header.go.txt" output:object:artifacts:config=/dev/null
 package v1alpha1
