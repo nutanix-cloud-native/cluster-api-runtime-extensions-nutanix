f71db2e391364e1ece1c50495072afb20e99170d
faiq <faiqrazarizvi@gmail.com>
2023-10-02 13:14:24 -0700

    ci: adds build process

--- make/addons.mk
+++ make/addons.mk
@@ -3,9 +3,10 @@
 
 export CALICO_VERSION := v3.26.1
 export NODE_FEATURE_DISCOVERY_VERSION := 0.14.1
+export AWS_CSI_SNAPSHOT_CONTROLLER_VERSION := v6.3.0
 
 .PHONY: addons.sync
-addons.sync: $(addprefix update-addon.,calico nfd)
+addons.sync: $(addprefix update-addon.,calico nfd aws-csi)
 
 .PHONY: update-addon.calico
 update-addon.calico: ; $(info $(M) updating calico manifests)
@@ -14,3 +15,7 @@ update-addon.calico: ; $(info $(M) updating calico manifests)
 .PHONY: update-addon.nfd
 update-addon.nfd: ; $(info $(M) updating node feature discovery manifests)
 	./hack/addons/update-node-feature-discovery-manifests.sh
+
+.PHONY: update-addon.aws-ebs-csi
+update-addon.aws-csi: ; $(info $(M) updating aws ebs csi manifests)
+	./hack/addons/update-aws-ebs-csi.sh
