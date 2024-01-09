59d29fea4209ac24ff34c44e28cddd544f65f4b0
Jimmi Dyson <jimmidyson@gmail.com>
2024-01-09 17:13:14 +0000

    feat: Pull in CAAPH APIs
    This is readying the project to utilise CAAPH for addon management.

---
+++ hack/third-party/caaph/provider.go
@@ -0,0 +1,8 @@
+// Copyright 2023 D2iQ, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	_ "sigs.k8s.io/cluster-api-addon-provider-helm/api/v1alpha1"
+)
