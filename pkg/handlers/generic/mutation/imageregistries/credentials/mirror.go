287b9acce75bef0948d1ddd11d6119057891d8ea
Shalin Patel <shalin.patel@nutanix.com>
2024-01-12 14:59:42 -0800

    fix: set mirror for image registry

--- pkg/handlers/generic/mutation/imageregistries/credentials/mirror.go
+++ pkg/handlers/generic/mutation/imageregistries/credentials/mirror.go
@@ -5,11 +5,14 @@ package credentials
 
 import (
 	"bytes"
+	"context"
 	_ "embed"
 	"fmt"
 	"text/template"
 
+	corev1 "k8s.io/api/core/v1"
 	cabpkv1 "sigs.k8s.io/cluster-api/bootstrap/kubeadm/api/v1beta1"
+	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/d2iq-labs/capi-runtime-extensions/api/v1alpha1"
 )
@@ -23,10 +26,72 @@ const (
 //go:embed templates/hosts.toml.gotmpl
 var defaultRegistryMirrorPatch []byte
 
+type mirrorConfig struct {
+	URL    string
+	CACert string
+}
+
+func mirrorFromImageRegistry(
+	ctx context.Context,
+	c ctrlclient.Client,
+	imageRegistry v1alpha1.ImageRegistry,
+	obj ctrlclient.Object,
+) (mirrorConfig, error) {
+	mirrorWithOptionalCACert := mirrorConfig{
+		URL: imageRegistry.URL,
+	}
+	secret, err := secretForMirrorCACert(
+		ctx,
+		c,
+		imageRegistry,
+		obj.GetNamespace(),
+	)
+	if err != nil {
+		return mirrorConfig{}, fmt.Errorf(
+			"error getting secret %s/%s from Image Registry variable: %w",
+			obj.GetNamespace(),
+			imageRegistry.Mirror.SecretRef.Name,
+			err,
+		)
+	}
+
+	if secret != nil {
+		mirrorWithOptionalCACert.CACert = string(secret.Data[secretKeyForMirrorCACert])
+	}
+
+	return mirrorWithOptionalCACert, nil
+}
+
+// secretForMirrorCACert returns the Secret for the given mirror's CA certificate.
+// Returns nil if the secret field is empty.
+func secretForMirrorCACert(
+	ctx context.Context,
+	c ctrlclient.Reader,
+	registry v1alpha1.ImageRegistry,
+	objectNamespace string,
+) (*corev1.Secret, error) {
+	if registry.Mirror == nil || registry.Mirror.SecretRef == nil {
+		return nil, nil
+	}
+
+	namespace := objectNamespace
+	if registry.Mirror.SecretRef.Namespace != "" {
+		namespace = registry.Mirror.SecretRef.Namespace
+	}
+
+	key := ctrlclient.ObjectKey{
+		Name:      registry.Mirror.SecretRef.Name,
+		Namespace: namespace,
+	}
+	secret := &corev1.Secret{}
+	err := c.Get(ctx, key, secret)
+	return secret, err
+}
+
 // Default Mirror for all registries. Use a mirror regardless of the intended registry.
 // The upstream registry will be automatically used after all defined mirrors have been tried.
 // reference: https://github.com/containerd/containerd/blob/main/docs/hosts.md#setup-default-mirror-for-all-registries
-func generateDefaultRegistryMirrorFile(config providerConfig) ([]cabpkv1.File, error) {
+func generateDefaultRegistryMirrorFile(mirror mirrorConfig) ([]cabpkv1.File, error) {
 	t, err := template.New("").Parse(string(defaultRegistryMirrorPatch))
 	if err != nil {
 		return nil, fmt.Errorf("fail to parse go template for registry mirror: %w", err)
@@ -35,11 +100,11 @@ func generateDefaultRegistryMirrorFile(config providerConfig) ([]cabpkv1.File, e
 		URL        string
 		CACertPath string
 	}{
-		URL: config.URL,
+		URL: mirror.URL,
 	}
 	// CA cert is optional for mirror registry.
 	// i.e. registry is using signed certificates. Insecure registry will not be allowed.
-	if config.CACert != "" {
+	if mirror.CACert != "" {
 		templateInput.CACertPath = mirrorCACertPathOnRemote
 	}
 
@@ -58,7 +123,7 @@ func generateDefaultRegistryMirrorFile(config providerConfig) ([]cabpkv1.File, e
 }
 
 func generateMirrorCACertFile(
-	config providerConfig,
+	config mirrorConfig,
 	registry v1alpha1.ImageRegistry,
 ) []cabpkv1.File {
 	if config.CACert == "" {
@@ -70,7 +135,7 @@ func generateMirrorCACertFile(
 			Permissions: "0600",
 			ContentFrom: &cabpkv1.FileSource{
 				Secret: cabpkv1.SecretFileSource{
-					Name: registry.CredentialsSecret.Name,
+					Name: registry.Mirror.SecretRef.Name,
 					Key:  secretKeyForMirrorCACert,
 				},
 			},
