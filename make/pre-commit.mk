5e0ad3110dece63d433f8ed6bbf4801584069304
Dimitri Koshkin <dimitri.koshkin@nutanix.com>
2024-02-06 10:51:57 -0800

    build: workaround pre-commit issue

--- make/pre-commit.mk
+++ make/pre-commit.mk
@@ -13,6 +13,8 @@ pre-commit: ; $(info $(M) running pre-commit)
 ifeq ($(wildcard $(PRE_COMMIT_CONFIG_FILE)),)
 	$(error Cannot find pre-commit config file $(PRE_COMMIT_CONFIG_FILE). Specify the config file via PRE_COMMIT_CONFIG_FILE variable)
 endif
+	# Set pip version to work around https://github.com/pypa/pip/issues/12372
+	env VIRTUALENV_PIP=24.0 pre-commit install-hooks
 	env SKIP=$(SKIP) pre-commit run -a --show-diff-on-failure --config $(PRE_COMMIT_CONFIG_FILE)
 	git fetch origin main
 	pre-commit run --hook-stage manual gitlint-ci
