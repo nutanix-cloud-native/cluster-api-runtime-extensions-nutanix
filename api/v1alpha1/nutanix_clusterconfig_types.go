a2c80fbc154f432c84f0f79d9a61c197693fd950
Deepak Muley <deepak.muley@nutanix.com>
2024-02-29 10:06:39 -0800

    WIP added all the types as a placeholder to share across team members

--- api/v1alpha1/nutanix_clusterconfig_types.go
+++ api/v1alpha1/nutanix_clusterconfig_types.go
@@ -8,7 +8,9 @@ import (
 )
 
 type NutanixSpec struct {
+	PrismCentralEndpoint *NutanixPrismCentralEndpointSpec `json:"prismCentralEndpoint,omitempty"`
 	ControlPlaneEndpoint *NutanixControlPlaneEndpointSpec `json:"controlPlaneEndpoint,omitempty"`
+	FailureDomains       []NutanixFailureDomain           `json:"faultDomains,omitempty"`
 }
 
 func (NutanixSpec) VariableSchema() clusterv1.VariableSchema {
@@ -17,7 +19,43 @@ func (NutanixSpec) VariableSchema() clusterv1.VariableSchema {
 			Description: "Nutanix cluster configuration",
 			Type:        "object",
 			Properties: map[string]clusterv1.JSONSchemaProps{
+				"prismCentralEndpoint": NutanixPrismCentralEndpointSpec{}.VariableSchema().OpenAPIV3Schema,
 				"controlPlaneEndpoint": NutanixControlPlaneEndpointSpec{}.VariableSchema().OpenAPIV3Schema,
+				// TODO "failureDomains": []NutanixFailureDomain{}.VariableSchema().OpenAPIV3Schema,
+			},
+		},
+	}
+}
+
+type NutanixPrismCentralEndpointSpec struct {
+	Host                  string `json:"host,omitempty"`
+	Port                  int32  `json:"port,omitempty"`
+	Insecure              bool   `json:"insecure,omitempty"`
+	AdditionalTrustBundle string `json:"additionalTrustBundle,omitempty"`
+}
+
+func (NutanixPrismCentralEndpointSpec) VariableSchema() clusterv1.VariableSchema {
+	return clusterv1.VariableSchema{
+		OpenAPIV3Schema: clusterv1.JSONSchemaProps{
+			Description: "Nutanix Prism Central endpoint configuration",
+			Type:        "object",
+			Properties: map[string]clusterv1.JSONSchemaProps{
+				"host": {
+					Description: "host ip/fqdn for Prism Central Server",
+					Type:        "string",
+				},
+				"port": {
+					Description: "port for Prism Central Server",
+					Type:        "integer",
+				},
+				"insecure": {
+					Description: "Prism Central Certificate checking",
+					Type:        "boolean",
+				},
+				"additionalTrustBundle": {
+					Description: "Certificate trust bundle used for Prism Central connection",
+					Type:        "string",
+				},
 			},
 		},
 	}
@@ -43,6 +81,47 @@ func (NutanixControlPlaneEndpointSpec) VariableSchema() clusterv1.VariableSchema
 					Type:        "integer",
 				},
 			},
+		},
+	}
+}
+
+type NutanixFailureDomain struct {
+	// name defines the unique name of a failure domain.
+	// Name is required and must be at most 64 characters in length.
+	// It must consist of only lower case alphanumeric characters and hyphens (-).
+	// It must start and end with an alphanumeric character.
+	// This value is arbitrary and is used to identify the failure domain within the platform.
+	Name string `json:"name"`
+
+	// cluster is to identify the cluster (the Prism Element under management of the Prism Central),
+	// in which the Machine's VM will be created. The cluster identifier (uuid or name) can be obtained
+	// from the Prism Central console or using the prism_central API.
+	Cluster NutanixResourceIdentifier `json:"cluster"`
+
+	// subnets holds a list of identifiers (one or more) of the cluster's network subnets
+	// for the Machine's VM to connect to. The subnet identifiers (uuid or name) can be
+	// obtained from the Prism Central console or using the prism_central API.
+	Subnets []NutanixResourceIdentifier `json:"subnets"`
+
+	// indicates if a failure domain is suited for control plane nodes
+	ControlPlane bool `json:"controlPlane,omitempty"`
+}
+
+func (NutanixFailureDomain) VariableSchema() clusterv1.VariableSchema {
+	return clusterv1.VariableSchema{
+		OpenAPIV3Schema: clusterv1.JSONSchemaProps{
+			Description: "Nutanix Failure Domain",
+			Type:        "object",
+			Properties:  map[string]clusterv1.JSONSchemaProps{
+				"name": {
+					Description: "name of failure domain",
+					Type:        "string",
+				},
+				"cluster": {
+					Description: "",
+					Type:        "integer",
+				},
+},
 		},
 	}
 }
--- api/v1alpha1/nutanix_node_types.go
+++ api/v1alpha1/nutanix_node_types.go
@@ -4,16 +4,132 @@
 package v1alpha1
 
 import (
+	"k8s.io/apimachinery/pkg/api/resource"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 )
 
+// NutanixIdentifierType is an enumeration of different resource identifier types.
+type NutanixIdentifierType string
+
+// NutanixBootType is an enumeration of different boot types.
+type NutanixBootType string
+
+// NutanixGPUIdentifierType is an enumeration of different resource identifier types for GPU entities.
+type NutanixGPUIdentifierType string
+
 type NutanixNodeSpec struct {
+	// vcpusPerSocket is the number of vCPUs per socket of the VM
+	VCPUsPerSocket int32 `json:"vcpusPerSocket"`
+
+	// vcpuSockets is the number of vCPU sockets of the VM
+	VCPUSockets int32 `json:"vcpuSockets"`
+
+	// memorySize is the memory size (in Quantity format) of the VM
+	MemorySize resource.Quantity `json:"memorySize"`
+
+	// image is to identify the rhcos image uploaded to the Prism Central (PC)
+	// The image identifier (uuid or name) can be obtained from the Prism Central console
+	// or using the prism_central API.
+	Image NutanixResourceIdentifier `json:"image"`
+
+	// cluster is to identify the cluster (the Prism Element under management
+	// of the Prism Central), in which the Machine's VM will be created.
+	// The cluster identifier (uuid or name) can be obtained from the Prism Central console
+	// or using the prism_central API.
+	Cluster NutanixResourceIdentifier `json:"cluster"`
+
+	// subnet is to identify the cluster's network subnet to use for the Machine's VM
+	// The cluster identifier (uuid or name) can be obtained from the Prism Central console
+	// or using the prism_central API.
+	Subnets []NutanixResourceIdentifier `json:"subnet"`
+
+	// List of categories that need to be added to the machines. Categories must already exist in Prism Central
+	AdditionalCategories []NutanixCategoryIdentifier `json:"additionalCategories,omitempty"`
+
+	// Add the machine resources to a Prism Central project
+	Project *NutanixResourceIdentifier `json:"project,omitempty"`
+
+	// Defines the boot type of the virtual machine. Only supports UEFI and Legacy
+	BootType string `json:"bootType,omitempty"` //TODO use enum NutanixBootType
+
+	// systemDiskSize is size (in Quantity format) of the system disk of the VM
+	// The minimum systemDiskSize is 20Gi bytes
+	SystemDiskSize resource.Quantity `json:"systemDiskSize"`
+
+	// List of GPU devices that need to be added to the machines.
+	GPUs []NutanixGPU `json:"gpus,omitempty"`
 }
 
 func (NutanixNodeSpec) VariableSchema() clusterv1.VariableSchema {
 	return clusterv1.VariableSchema{
 		OpenAPIV3Schema: clusterv1.JSONSchemaProps{
 			Description: "Nutanix Node configuration",
+			Type:        "object",
+			Properties:  map[string]clusterv1.JSONSchemaProps{},
+		},
+	}
+}
+
+type NutanixResourceIdentifier struct {
+	// Type is the identifier type to use for this resource.
+	Type NutanixIdentifierType `json:"type"`
+
+	// uuid is the UUID of the resource in the PC.
+	// +optional
+	UUID *string `json:"uuid,omitempty"`
+
+	// name is the resource name in the PC
+	// +optional
+	Name *string `json:"name,omitempty"`
+}
+
+func (NutanixResourceIdentifier) VariableSchema() clusterv1.VariableSchema {
+	return clusterv1.VariableSchema{
+		OpenAPIV3Schema: clusterv1.JSONSchemaProps{
+			Description: "Nutanix Resource Identifier",
+			Type:        "object",
+			Properties:  map[string]clusterv1.JSONSchemaProps{},
+		},
+	}
+}
+
+type NutanixCategoryIdentifier struct {
+	// key is the Key of category in PC.
+	// +optional
+	Key string `json:"key,omitempty"`
+
+	// value is the category value linked to the category key in PC
+	// +optional
+	Value string `json:"value,omitempty"`
+}
+
+func (NutanixCategoryIdentifier) VariableSchema() clusterv1.VariableSchema {
+	return clusterv1.VariableSchema{
+		OpenAPIV3Schema: clusterv1.JSONSchemaProps{
+			Description: "Nutanix Category Identifier",
+			Type:        "object",
+			Properties:  map[string]clusterv1.JSONSchemaProps{},
+		},
+	}
+}
+
+type NutanixGPU struct {
+	// Type is the identifier type to use for this resource.
+	Type NutanixGPUIdentifierType `json:"type"`
+
+	// deviceID is the id of the GPU entity.
+	// +optional
+	DeviceID *int64 `json:"deviceID,omitempty"`
+
+	// name is the GPU name
+	// +optional
+	Name *string `json:"name,omitempty"`
+}
+
+func (NutanixGPU) VariableSchema() clusterv1.VariableSchema {
+	return clusterv1.VariableSchema{
+		OpenAPIV3Schema: clusterv1.JSONSchemaProps{
+			Description: "Nutanix GPU type",
 			Type:        "object",
 			Properties:  map[string]clusterv1.JSONSchemaProps{},
 		},
