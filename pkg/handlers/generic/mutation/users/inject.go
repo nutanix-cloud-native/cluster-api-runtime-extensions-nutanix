08f86e4126cd950bbcc0b605bbff9576255b9928
Daniel Lipovetsky <daniel.lipovetsky@nutanix.com>
2024-03-18 09:46:56 -0700

    feat: Add user configuration for all providers
    - Defines a cluster-level variable for defining one or more users
    - Patches bootstrap templates for control plane and worker node pools
      with user configuration

---
+++ pkg/handlers/generic/mutation/users/inject.go
@@ -0,0 +1,126 @@
+// Copyright 2023 D2iQ, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package users
+
+import (
+	"context"
+
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	bootstrapv1 "sigs.k8s.io/cluster-api/bootstrap/kubeadm/api/v1beta1"
+	controlplanev1 "sigs.k8s.io/cluster-api/controlplane/kubeadm/api/v1beta1"
+	runtimehooksv1 "sigs.k8s.io/cluster-api/exp/runtime/hooks/api/v1alpha1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/d2iq-labs/cluster-api-runtime-extensions-nutanix/api/v1alpha1"
+	"github.com/d2iq-labs/cluster-api-runtime-extensions-nutanix/common/pkg/capi/clustertopology/patches"
+	"github.com/d2iq-labs/cluster-api-runtime-extensions-nutanix/common/pkg/capi/clustertopology/patches/selectors"
+	"github.com/d2iq-labs/cluster-api-runtime-extensions-nutanix/common/pkg/capi/clustertopology/variables"
+	"github.com/d2iq-labs/cluster-api-runtime-extensions-nutanix/pkg/handlers/generic/clusterconfig"
+)
+
+const (
+	// VariableName is the external patch variable name.
+	VariableName = "users"
+)
+
+type usersPatchHandler struct {
+	variableName      string
+	variableFieldPath []string
+}
+
+func NewPatch() *usersPatchHandler {
+	return newUsersPatchHandler(
+		clusterconfig.MetaVariableName,
+		VariableName)
+}
+
+func newUsersPatchHandler(
+	variableName string,
+	variableFieldPath ...string,
+) *usersPatchHandler {
+	return &usersPatchHandler{
+		variableName:      variableName,
+		variableFieldPath: variableFieldPath,
+	}
+}
+
+func (h *usersPatchHandler) Mutate(
+	ctx context.Context,
+	obj *unstructured.Unstructured,
+	vars map[string]apiextensionsv1.JSON,
+	holderRef runtimehooksv1.HolderReference,
+	_ ctrlclient.ObjectKey,
+) error {
+	log := ctrl.LoggerFrom(ctx, "holderRef", holderRef)
+
+	usersVariable, found, err := variables.Get[v1alpha1.Users](
+		vars,
+		h.variableName,
+		h.variableFieldPath...,
+	)
+	if err != nil {
+		return err
+	}
+	if !found {
+		log.V(5).Info("users variable not defined")
+		return nil
+	}
+
+	log = log.WithValues(
+		"variableName",
+		h.variableName,
+		"variableFieldPath",
+		h.variableFieldPath,
+		"variableValue",
+		usersVariable,
+	)
+
+	if err := patches.MutateIfApplicable(
+		obj, vars, &holderRef, selectors.ControlPlane(), log,
+		func(obj *controlplanev1.KubeadmControlPlaneTemplate) error {
+			log.WithValues(
+				"patchedObjectKind", obj.GetObjectKind().GroupVersionKind().String(),
+				"patchedObjectName", ctrlclient.ObjectKeyFromObject(obj),
+			).Info("setting users in control plane kubeadm config template")
+			bootstrapUsers := []bootstrapv1.User{}
+			for _, userFromVariable := range usersVariable {
+				bootstrapUsers = append(bootstrapUsers, generateBootstrapUser(userFromVariable))
+			}
+			obj.Spec.Template.Spec.KubeadmConfigSpec.Users = bootstrapUsers
+			return nil
+		}); err != nil {
+		return err
+	}
+
+	if err := patches.MutateIfApplicable(
+		obj, vars, &holderRef, selectors.WorkersKubeadmConfigTemplateSelector(), log,
+		func(obj *bootstrapv1.KubeadmConfigTemplate) error {
+			log.WithValues(
+				"patchedObjectKind", obj.GetObjectKind().GroupVersionKind().String(),
+				"patchedObjectName", ctrlclient.ObjectKeyFromObject(obj),
+			).Info("setting users in worker node kubeadm config template")
+			bootstrapUsers := []bootstrapv1.User{}
+			for _, userFromVariable := range usersVariable {
+				bootstrapUsers = append(bootstrapUsers, generateBootstrapUser(userFromVariable))
+			}
+			obj.Spec.Template.Spec.Users = bootstrapUsers
+			return nil
+		}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func generateBootstrapUser(userFromVariable v1alpha1.User) bootstrapv1.User {
+	return bootstrapv1.User{
+		Name:              userFromVariable.Name,
+		LockPassword:      userFromVariable.LockPassword,
+		Passwd:            userFromVariable.Passwd,
+		SSHAuthorizedKeys: userFromVariable.SSHAuthorizedKeys,
+		Sudo:              userFromVariable.Sudo,
+	}
+}
