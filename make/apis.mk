f09e934848bac09b017ef6812cbceb7d2766cb1e
Daniel Lipovetsky <daniel.lipovetsky@nutanix.com>
2024-03-18 12:40:35 -0700

    fix: Remove vendored types for core CAPI providers (CAPD, CABPK, KCP)

--- make/apis.mk
+++ make/apis.mk
@@ -1,13 +1,29 @@
 # Copyright 2023 D2iQ, Inc. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+# We import sigs.k8s.io/cluster-api, which itself imports sigs.k8s.io/controller-runtime,
+# as well as other modules.
+#
+# Third-party CAPI providers, e.g. CAPA, CAPZ, etc, also depend on these modules, but
+# usually at different versions. For this reason, importing both sigs.k8s.io/cluster-api
+# and a third-party provider usually causes dependency conflicts.
+#
+# To avoid conflicts, we do not import third-party provider types. Instead, we "sync,"
+# i.e. copy, or vendor, the third-party provider types into the api/external directory.
+#
+# However, we do not sync the Kubeadm Control Plane or Kubeadm Bootstrap provider types,
+# because they are developed in the sigs.k8s.io/cluster-api module, which we define as
+# a dependency.
+#
+# We also do not sync the Docker infrastructure provider types, because they in the
+# sigs.k8s.io/cluster-api/test module, which is developed in parallel with
+# sigs.k8s.io/cluster-api. For that reason, we expect no dependency conflicts.
+
+# Add third-party CAPI provider types below
+
 PROVIDER_MODULE_capa := sigs.k8s.io/cluster-api-provider-aws/v2
 PROVIDER_API_PATH_capa := api
 PROVIDER_API_VERSION_capa := v1beta2
-
-PROVIDER_MODULE_capd := sigs.k8s.io/cluster-api/test
-PROVIDER_API_PATH_capd := infrastructure/docker/api
-PROVIDER_API_VERSION_capd := v1beta1
 
 PROVIDER_MODULE_caaph := sigs.k8s.io/cluster-api-addon-provider-helm
 PROVIDER_API_PATH_caaph := api
@@ -17,19 +33,14 @@ PROVIDER_MODULE_capx := github.com/nutanix-cloud-native/cluster-api-provider-nut
 PROVIDER_API_PATH_capx := api
 PROVIDER_API_VERSION_capx := v1beta1
 
-PROVIDER_MODULE_cabpk := sigs.k8s.io/cluster-api
-PROVIDER_API_PATH_cabpk := bootstrap/kubeadm/api
-PROVIDER_API_VERSION_cabpk := v1beta1
+# Add third-party CAPI provider types above
 
-# It is not possible to resolve Kubernetes and controller-runtime modules for the different infrastructure providers
-# without hitting dependency conflicts.
-# Instead. sync their APIs into the api/external directory.
 .PHONY: apis.sync
-apis.sync: ## Syncs infrastructure providers' APIs
-apis.sync: $(addprefix api.sync.,capa capd caaph capx cabpk) go-fix.api mod-tidy.api
+apis.sync: ## Syncs third-party CAPI providers' types
+apis.sync: $(addprefix api.sync.,capa caaph capx) go-fix.api mod-tidy.api
 
 .PHONY: api.sync.%
-api.sync.%: ## Syncs an infrastructure provider's API
+api.sync.%: ## Syncs a third-party CAPI provider's API types
 api.sync.%: PROVIDER_MODULE_DIR=$(REPO_ROOT)/hack/third-party/$*
 api.sync.%: PROVIDER_API_DIR=api/external/$(PROVIDER_MODULE_$*)/$(PROVIDER_API_PATH_$*)/$(PROVIDER_API_VERSION_$*)/
 api.sync.%: ; $(info $(M) syncing external API: $(PROVIDER_MODULE_$*)/$(PROVIDER_API_PATH_$*)/$(PROVIDER_API_VERSION_$*))
