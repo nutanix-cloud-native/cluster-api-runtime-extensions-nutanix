d9f63446a074e58f4355f7feb73d4955cf9f42b9
Daniel Lipovetsky <daniel.lipovetsky@nutanix.com>
2024-03-18 13:57:51 -0700

    fix: Remove vendored types for core CAPI providers (CAPD, CABPK, KCP) (#452)
    We do not need to sync the Kubeadm Control Plane or Kubeadm Bootstrap
    provider types, because they are developed in the sigs.k8s.io/cluster-api module, which
    we define as a dependency.
    
    We also do not need to sync the Docker infrastructure provider types,
    because they in the sigs.k8s.io/cluster-api/test module, which is developed
    in parallel with sigs.k8s.io/cluster-api. For that reason, we expect no dependency
    conflicts.

--- pkg/handlers/docker/mutation/customimage/inject_worker.go
+++ pkg/handlers/docker/mutation/customimage/inject_worker.go
@@ -11,10 +11,10 @@ import (
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	runtimehooksv1 "sigs.k8s.io/cluster-api/exp/runtime/hooks/api/v1alpha1"
+	capdv1 "sigs.k8s.io/cluster-api/test/infrastructure/docker/api/v1beta1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	capdv1 "github.com/d2iq-labs/capi-runtime-extensions/api/external/sigs.k8s.io/cluster-api/test/infrastructure/docker/api/v1beta1"
 	"github.com/d2iq-labs/capi-runtime-extensions/api/v1alpha1"
 	"github.com/d2iq-labs/capi-runtime-extensions/common/pkg/capi/clustertopology/patches"
 	"github.com/d2iq-labs/capi-runtime-extensions/common/pkg/capi/clustertopology/patches/selectors"
