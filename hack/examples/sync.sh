c5384877cf6f4afcce3e306e658667c95047b757
Jimmi Dyson <jimmidyson@gmail.com>
2024-03-19 19:13:32 +0000

    build: Reorg example kustomizations (#453)
    Reorganised the example kustomization following @deepakm-ntnx's
    comments.

--- hack/examples/sync.sh
+++ hack/examples/sync.sh
@@ -19,92 +19,25 @@ mkdir -p "${EXAMPLE_CLUSTERCLASSES_DIR}"
 readonly EXAMPLE_CLUSTERS_DIR=examples/capi-quick-start
 mkdir -p "${EXAMPLE_CLUSTERS_DIR}"
 
-# Sync ClusterClasses (including Templates) and Clusters to separate files.
-kustomize build ./hack/examples |
-  tee \
-    >(
-      gojq --yaml-input --yaml-output 'select(.metadata.labels["cluster.x-k8s.io/provider"] == "docker"
-                                        and .kind != "Cluster"
-                                        and .kind != "DockerMachinePoolTemplate"
-                                      )' >"${EXAMPLE_CLUSTERCLASSES_DIR}/docker-cluster-class.yaml"
-    ) \
-    >(
-      gojq --yaml-input --yaml-output 'select(
-                                        .metadata.labels["cluster.x-k8s.io/provider"] == "docker"
-                                        and .kind == "Cluster"
-                                        and .spec.topology.variables[0].value.addons.cni.provider == "Cilium"
-                                        and .spec.topology.variables[0].value.addons.cni.strategy == "ClusterResourceSet"
-                                      )' >"${EXAMPLE_CLUSTERS_DIR}/docker-cluster-cilium-crs.yaml"
-    ) \
-    >(
-      gojq --yaml-input --yaml-output 'select(
-                                        .metadata.labels["cluster.x-k8s.io/provider"] == "docker"
-                                        and .kind == "Cluster"
-                                        and .spec.topology.variables[0].value.addons.cni.provider == "Cilium"
-                                        and .spec.topology.variables[0].value.addons.cni.strategy == "HelmAddon"
-                                      )' >"${EXAMPLE_CLUSTERS_DIR}/docker-cluster-cilium-helm-addon.yaml"
-    ) \
-    >(
-      gojq --yaml-input --yaml-output 'select(
-                                        .metadata.labels["cluster.x-k8s.io/provider"] == "docker"
-                                        and .kind == "Cluster"
-                                        and .spec.topology.variables[0].value.addons.cni.provider == "Calico"
-                                        and .spec.topology.variables[0].value.addons.cni.strategy == "ClusterResourceSet"
-                                      )' >"${EXAMPLE_CLUSTERS_DIR}/docker-cluster-calico-crs.yaml"
-    ) \
-    >(
-      gojq --yaml-input --yaml-output 'select(
-                                        .metadata.labels["cluster.x-k8s.io/provider"] == "docker"
-                                        and .kind == "Cluster"
-                                        and .spec.topology.variables[0].value.addons.cni.provider == "Calico"
-                                        and .spec.topology.variables[0].value.addons.cni.strategy == "HelmAddon"
-                                      )' >"${EXAMPLE_CLUSTERS_DIR}/docker-cluster-calico-helm-addon.yaml"
-    ) \
-    >(
-      gojq --yaml-input --yaml-output 'select(.metadata.labels["cluster.x-k8s.io/provider"] == "aws"
-                                        and .kind != "Cluster"
-                                        and .kind != "AWSClusterStaticIdentity"
-                                      )' >"${EXAMPLE_CLUSTERCLASSES_DIR}/aws-cluster-class.yaml"
-    ) \
-    >(
-      gojq --yaml-input --yaml-output 'select(.metadata.labels["cluster.x-k8s.io/provider"] == "aws"
-                                        and .kind == "Cluster"
-                                        and .spec.topology.variables[0].value.addons.cni.provider == "Calico"
-                                        and .spec.topology.variables[0].value.addons.cni.strategy == "ClusterResourceSet"
-                                      )' >"${EXAMPLE_CLUSTERS_DIR}/aws-cluster-calico-crs.yaml"
-    ) \
-    >(
-      gojq --yaml-input --yaml-output 'select(.metadata.labels["cluster.x-k8s.io/provider"] == "aws"
-                                        and .kind == "Cluster"
-                                        and .spec.topology.variables[0].value.addons.cni.provider == "Calico"
-                                        and .spec.topology.variables[0].value.addons.cni.strategy == "HelmAddon"
-                                      )' >"${EXAMPLE_CLUSTERS_DIR}/aws-cluster-calico-helm-addon.yaml"
-    ) \
-    >(
-      gojq --yaml-input --yaml-output 'select(.metadata.labels["cluster.x-k8s.io/provider"] == "aws"
-                                        and .kind == "Cluster"
-                                        and .spec.topology.variables[0].value.addons.cni.provider == "Cilium"
-                                        and .spec.topology.variables[0].value.addons.cni.strategy == "ClusterResourceSet"
-                                      )' >"${EXAMPLE_CLUSTERS_DIR}/aws-cluster-cilium-crs.yaml"
-    ) \
-    >(
-      gojq --yaml-input --yaml-output 'select(.metadata.labels["cluster.x-k8s.io/provider"] == "aws"
-                                        and .kind == "Cluster"
-                                        and .spec.topology.variables[0].value.addons.cni.provider == "Cilium"
-                                        and .spec.topology.variables[0].value.addons.cni.strategy == "HelmAddon"
-                                      )' >"${EXAMPLE_CLUSTERS_DIR}/aws-cluster-cilium-helm-addon.yaml"
-    ) \
-    >/dev/null
+kustomize build ./hack/examples/overlays/clusterclasses/aws >"${EXAMPLE_CLUSTERCLASSES_DIR}/aws-cluster-class.yaml"
+kustomize build ./hack/examples/overlays/clusterclasses/docker >"${EXAMPLE_CLUSTERCLASSES_DIR}/docker-cluster-class.yaml"
 
-#shellcheck disable=SC2016
-sed -i'' 's/^  name: .\+$/  name: ${CLUSTER_NAME}/' "${EXAMPLE_CLUSTERS_DIR}"/*.yaml
+for provider in "aws" "docker"; do
+  kustomize build --load-restrictor LoadRestrictionsNone \
+    ./hack/examples/overlays/clusterclasses/"${provider}" >"${EXAMPLE_CLUSTERCLASSES_DIR}"/"${provider}"-cluster-class.yaml
+
+  for cni in "calico" "cilium"; do
+    for strategy in "helm-addon" "crs"; do
+      kustomize build --load-restrictor LoadRestrictionsNone \
+        ./hack/examples/overlays/clusters/"${provider}"/"${cni}"/"${strategy}" \
+        >"${EXAMPLE_CLUSTERS_DIR}/${provider}-cluster-${cni}-${strategy}.yaml"
+    done
+  done
+done
 
 # TODO Remove once kustomize supports retaining quotes in what will be numeric values.
 #shellcheck disable=SC2016
 sed -i'' 's/${AMI_LOOKUP_ORG}/"${AMI_LOOKUP_ORG}"/' "${EXAMPLE_CLUSTERS_DIR}"/*.yaml
-
-# TODO Remove once CAPA templates default to using external cloud provider.
-sed -i'' 's/cloud-provider:\ aws/cloud-provider:\ external/g' "${EXAMPLE_CLUSTERCLASSES_DIR}/aws-cluster-class.yaml"
 
 # TODO Remove once kustomize supports retaining quotes in what will be numeric values.
 #shellcheck disable=SC2016
