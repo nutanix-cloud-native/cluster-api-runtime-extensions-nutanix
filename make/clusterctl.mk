8d8d005f551f03ca9c0260a0dbeaa1086c54beb0
Jimmi Dyson <jimmi.dyson@nutanix.com>
2024-04-10 16:32:37 +0100

    build: Specify CAPI provider versions from go module versions (#47)
    This makes the deployment consistent as well as avoiding issues when providers
    are in the middle of a release.

--- make/clusterctl.mk
+++ make/clusterctl.mk
@@ -1,8 +1,11 @@
 # Copyright 2023 D2iQ, Inc. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+export CAPI_VERSION := $(shell go list -m -f '{{ .Version }}' sigs.k8s.io/cluster-api)
+export CAPD_VERSION := $(shell go list -m -f '{{ .Version }}' sigs.k8s.io/cluster-api/test)
 export CAPA_VERSION := $(shell cd hack/third-party/capa && go list -m -f '{{ .Version }}' sigs.k8s.io/cluster-api-provider-aws/v2)
 export CAPX_VERSION := $(shell cd hack/third-party/capx && go list -m -f '{{ .Version }}' github.com/nutanix-cloud-native/cluster-api-provider-nutanix)
+export CAAPH_VERSION := $(shell cd hack/third-party/caaph && go list -m -f '{{ .Version }}' sigs.k8s.io/cluster-api-addon-provider-helm)
 
 # Leave Nutanix credentials empty here and set it when creating the clusters
 .PHONY: clusterctl.init
@@ -15,8 +18,11 @@ clusterctl.init:
 	    NUTANIX_ENDPOINT="" NUTANIX_PASSWORD="" NUTANIX_USER="" \
 	    clusterctl init \
 	      --kubeconfig=$(KIND_KUBECONFIG) \
-	      --infrastructure docker,aws:${CAPA_VERSION},nutanix:${CAPX_VERSION} \
-	      --addon helm \
+	      --core cluster-api:$(CAPI_VERSION) \
+	      --bootstrap kubeadm:$(CAPI_VERSION) \
+	      --control-plane kubeadm:$(CAPI_VERSION) \
+	      --infrastructure docker:$(CAPD_VERSION),aws:$(CAPA_VERSION),nutanix:$(CAPX_VERSION) \
+	      --addon helm:$(CAAPH_VERSION) \
 	      --wait-providers
 
 .PHONY: clusterctl.delete
