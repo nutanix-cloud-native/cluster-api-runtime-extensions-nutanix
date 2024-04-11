58bb86c4775bfff96f45d26d99d30fdd67c91463
Shalin Patel <shalin.patel@nutanix.com>
2024-04-10 17:33:16 -0700

    build: update aws credentials on kind bootstrap cluster

--- make/dev.mk
+++ make/dev.mk
@@ -18,6 +18,8 @@ endif
 	kubectl rollout restart deployment cluster-api-runtime-extensions-nutanix
 	kubectl rollout status deployment cluster-api-runtime-extensions-nutanix
 
+.PHONY: dev.update-webhook-image-on-kind
+dev.update-webhook-image-on-kind: export KUBECONFIG := $(KIND_KUBECONFIG)
 dev.update-webhook-image-on-kind:
 ifndef SKIP_BUILD
 	$(MAKE) release-snapshot
@@ -27,3 +29,12 @@ endif
 	kubectl set image deployment cluster-api-runtime-extensions-nutanix webhook=ko.local/cluster-api-runtime-extensions-nutanix:$$(gojq -r .version dist/metadata.json)
 	kubectl rollout restart deployment cluster-api-runtime-extensions-nutanix
 	kubectl rollout status deployment cluster-api-runtime-extensions-nutanix
+
+
+.PHONY: dev.update-bootstrap-credentials-aws
+dev.update-bootstrap-credentials-aws: export KUBECONFIG := $(KIND_KUBECONFIG)
+dev.update-bootstrap-credentials-aws: export AWS_B64ENCODED_CREDENTIALS := $(shell clusterawsadm bootstrap credentials encode-as-profile)
+dev.update-bootstrap-credentials-aws:
+	kubectl patch secret capa-manager-bootstrap-credentials -n capa-system -p='{"data":{"credentials": "$(AWS_B64ENCODED_CREDENTIALS)"}}'
+	kubectl rollout restart deployment capa-controller-manager -n capa-system
+	kubectl rollout status deployment capa-controller-manager -n capa-system
