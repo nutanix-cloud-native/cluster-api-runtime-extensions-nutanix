e076a1d09bc5de6a9a2130d331fae56408eb7395
Dimitri Koshkin <dimitri.koshkin@nutanix.com>
2024-04-18 10:35:27 -0700

    refactor: nutanix variables test

--- pkg/handlers/nutanix/mutation/machinedetails/variables_test.go
+++ pkg/handlers/nutanix/mutation/machinedetails/variables_test.go
@@ -17,12 +17,46 @@ import (
 )
 
 func TestVariableValidation(t *testing.T) {
-	testImageName := "fake-image"
-	testPEClusterName := "fake-pe-cluster"
-	testProject := ptr.To(v1alpha1.NutanixResourceIdentifier{
-		Type: capxv1.NutanixIdentifierName,
-		Name: ptr.To("fake-project"),
-	})
+	requiredFields := minimumClusterConfigSpec()
+
+	withAdditionalCategories := minimumClusterConfigSpec()
+	//nolint:lll // gofumpt formats is this way
+	withAdditionalCategories.ControlPlane.Nutanix.MachineDetails.AdditionalCategories = []v1alpha1.NutanixCategoryIdentifier{
+		{
+			Key:   "fake-key",
+			Value: "fake-value1",
+		},
+		{
+			Key:   "fake-key",
+			Value: "fake-value2",
+		},
+	}
+
+	withProject := minimumClusterConfigSpec()
+	withProject.ControlPlane.Nutanix.MachineDetails.Project = ptr.To(
+		v1alpha1.NutanixResourceIdentifier{
+			Type: capxv1.NutanixIdentifierName,
+			Name: ptr.To("fake-project"),
+		},
+	)
+
+	invalidBootType := minimumClusterConfigSpec()
+	invalidBootType.ControlPlane.Nutanix.MachineDetails.BootType = "invalid-boot-type"
+
+	invalidImageType := minimumClusterConfigSpec()
+	invalidImageType.ControlPlane.Nutanix.MachineDetails.Image.Type = "invalid-image-type"
+
+	invalidClusterType := minimumClusterConfigSpec()
+	invalidClusterType.ControlPlane.Nutanix.MachineDetails.Cluster.Type = "invalid-cluster-type"
+
+	invalidProjectType := minimumClusterConfigSpec()
+	invalidProjectType.ControlPlane.Nutanix.MachineDetails.Project = ptr.To(
+		v1alpha1.NutanixResourceIdentifier{
+			Type: "invalid-project-type",
+			Name: ptr.To("fake-project"),
+		},
+	)
+
 	capitest.ValidateDiscoverVariables(
 		t,
 		clusterconfig.MetaVariableName,
@@ -30,118 +64,61 @@ func TestVariableValidation(t *testing.T) {
 		true,
 		nutanixclusterconfig.NewVariable,
 		capitest.VariableTestDef{
-			Name: "all fields set",
-			Vals: v1alpha1.ClusterConfigSpec{
-				ControlPlane: &v1alpha1.NodeConfigSpec{
-					Nutanix: &v1alpha1.NutanixNodeSpec{
-						MachineDetails: v1alpha1.NutanixMachineDetails{
-							BootType:       v1alpha1.NutanixBootType(capxv1.NutanixBootTypeLegacy),
-							VCPUSockets:    2,
-							VCPUsPerSocket: 1,
-							Image: v1alpha1.NutanixResourceIdentifier{
-								Type: capxv1.NutanixIdentifierName,
-								Name: &testImageName,
-							},
-							Cluster: v1alpha1.NutanixResourceIdentifier{
-								Type: capxv1.NutanixIdentifierName,
-								Name: &testPEClusterName,
-							},
-							MemorySize:     resource.MustParse("8Gi"),
-							Project:        testProject,
-							SystemDiskSize: resource.MustParse("40Gi"),
-							Subnets:        []v1alpha1.NutanixResourceIdentifier{},
-							AdditionalCategories: []v1alpha1.NutanixCategoryIdentifier{
-								{
-									Key:   "fake-key",
-									Value: "fake-value1",
-								},
-								{
-									Key:   "fake-key",
-									Value: "fake-value2",
-								},
-							},
-						},
-					},
-				},
-			},
+			Name: "required fields set",
+			Vals: requiredFields,
 		},
 		capitest.VariableTestDef{
-			Name: "invalid boot type",
-			Vals: v1alpha1.ClusterConfigSpec{
-				ControlPlane: &v1alpha1.NodeConfigSpec{
-					Nutanix: &v1alpha1.NutanixNodeSpec{
-						MachineDetails: v1alpha1.NutanixMachineDetails{
-							BootType:       "invalid",
-							VCPUSockets:    2,
-							VCPUsPerSocket: 1,
-							Image: v1alpha1.NutanixResourceIdentifier{
-								Type: capxv1.NutanixIdentifierName,
-								Name: &testImageName,
-							},
-							Cluster: v1alpha1.NutanixResourceIdentifier{
-								Type: capxv1.NutanixIdentifierName,
-								Name: &testPEClusterName,
-							},
-							MemorySize:     resource.MustParse("8Gi"),
-							SystemDiskSize: resource.MustParse("40Gi"),
-							Subnets:        []v1alpha1.NutanixResourceIdentifier{},
-						},
-					},
-				},
-			},
+			Name: "additional categories set",
+			Vals: withAdditionalCategories,
+		},
+		capitest.VariableTestDef{
+			Name: "project set",
+			Vals: withProject,
+		},
+		capitest.VariableTestDef{
+			Name:        "invalid boot type",
+			Vals:        invalidBootType,
 			ExpectError: true,
 		},
 		capitest.VariableTestDef{
-			Name: "invalid image type",
-			Vals: v1alpha1.ClusterConfigSpec{
-				ControlPlane: &v1alpha1.NodeConfigSpec{
-					Nutanix: &v1alpha1.NutanixNodeSpec{
-						MachineDetails: v1alpha1.NutanixMachineDetails{
-							BootType:       v1alpha1.NutanixBootType(capxv1.NutanixBootTypeLegacy),
-							VCPUSockets:    2,
-							VCPUsPerSocket: 1,
-							Image: v1alpha1.NutanixResourceIdentifier{
-								Type: "invalid",
-								Name: &testImageName,
-							},
-							Cluster: v1alpha1.NutanixResourceIdentifier{
-								Type: capxv1.NutanixIdentifierName,
-								Name: &testPEClusterName,
-							},
-							MemorySize:     resource.MustParse("8Gi"),
-							SystemDiskSize: resource.MustParse("40Gi"),
-							Subnets:        []v1alpha1.NutanixResourceIdentifier{},
-						},
-					},
-				},
-			},
+			Name:        "invalid image type",
+			Vals:        invalidImageType,
 			ExpectError: true,
 		},
 		capitest.VariableTestDef{
-			Name: "invalid cluster type",
-			Vals: v1alpha1.ClusterConfigSpec{
-				ControlPlane: &v1alpha1.NodeConfigSpec{
-					Nutanix: &v1alpha1.NutanixNodeSpec{
-						MachineDetails: v1alpha1.NutanixMachineDetails{
-							BootType:       v1alpha1.NutanixBootType(capxv1.NutanixBootTypeLegacy),
-							VCPUSockets:    2,
-							VCPUsPerSocket: 1,
-							Image: v1alpha1.NutanixResourceIdentifier{
-								Type: capxv1.NutanixIdentifierName,
-								Name: &testImageName,
-							},
-							Cluster: v1alpha1.NutanixResourceIdentifier{
-								Type: "invalid",
-								Name: &testPEClusterName,
-							},
-							MemorySize:     resource.MustParse("8Gi"),
-							SystemDiskSize: resource.MustParse("40Gi"),
-							Subnets:        []v1alpha1.NutanixResourceIdentifier{},
-						},
-					},
-				},
-			},
+			Name:        "invalid cluster type",
+			Vals:        invalidClusterType,
+			ExpectError: true,
+		},
+		capitest.VariableTestDef{
+			Name:        "invalid project type",
+			Vals:        invalidProjectType,
 			ExpectError: true,
 		},
 	)
+}
+
+func minimumClusterConfigSpec() v1alpha1.ClusterConfigSpec {
+	return v1alpha1.ClusterConfigSpec{
+		ControlPlane: &v1alpha1.NodeConfigSpec{
+			Nutanix: &v1alpha1.NutanixNodeSpec{
+				MachineDetails: v1alpha1.NutanixMachineDetails{
+					BootType:       v1alpha1.NutanixBootType(capxv1.NutanixBootTypeLegacy),
+					VCPUSockets:    2,
+					VCPUsPerSocket: 1,
+					Image: v1alpha1.NutanixResourceIdentifier{
+						Type: capxv1.NutanixIdentifierName,
+						Name: ptr.To("fake-image"),
+					},
+					Cluster: v1alpha1.NutanixResourceIdentifier{
+						Type: capxv1.NutanixIdentifierName,
+						Name: ptr.To("fake-pe-cluster"),
+					},
+					MemorySize:     resource.MustParse("8Gi"),
+					SystemDiskSize: resource.MustParse("40Gi"),
+					Subnets:        []v1alpha1.NutanixResourceIdentifier{},
+				},
+			},
+		},
+	}
 }
