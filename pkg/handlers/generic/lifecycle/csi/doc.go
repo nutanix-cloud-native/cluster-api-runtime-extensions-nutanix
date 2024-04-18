8d3091e0fd5e9ff942419daa47b9f835b0d88754
Jimmi Dyson <jimmidyson@gmail.com>
2024-04-19 00:27:53 +0100

    refactor: Move API to caren.nutanix.com group (#534)
    **What problem does this PR solve?**:
    
    **Which issue(s) this PR fixes**:
    Fixes #
    
    **How Has This Been Tested?**:
    <!--
    Please describe the tests that you ran to verify your changes.
    Provide output from the tests and any manual steps needed to replicate
    the tests.
    -->
    
    **Special notes for your reviewer**:
    <!--
    Use this to provide any additional information to the reviewers.
    This may include:
    - Best way to review the PR.
    - Where the author wants the most review attention on.
    - etc.
    -->

--- pkg/handlers/generic/lifecycle/csi/doc.go
+++ pkg/handlers/generic/lifecycle/csi/doc.go
@@ -4,7 +4,7 @@
 // Package calico provides a handler for managing Calico deployments on clusters, configurable via
 // labels and annotations.
 //
-// To enable Calico deployment, a cluster must be labelled with `capiext.labs.d2iq.io/cni=calico`.
+// To enable Calico deployment, a cluster must be labelled with `caren.nutanix.com/cni=calico`.
 // This will ensure the Tigera Configmap and associated ClusterResourceSet.
 //
 // +kubebuilder:rbac:groups=addons.cluster.x-k8s.io,resources=clusterresourcesets,verbs=watch;list;get;create;patch;update;delete
