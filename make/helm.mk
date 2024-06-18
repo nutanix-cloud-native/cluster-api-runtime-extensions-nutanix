9a4f0f3bfc9b23762e7b7c6c5af034e5b6be93e3
Daniel Lipovetsky <daniel.lipovetsky@nutanix.com>
2024-06-18 15:53:55 -0700

    build: Add make target to update helm chart values JSON schema

--- make/helm.mk
+++ make/helm.mk
@@ -1,6 +1,8 @@
 # Copyright 2023 Nutanix. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+HELM_PLUGINS=$(REPO_ROOT)/.local/helm/plugins
+
 .PHONY: chart-docs
 chart-docs: ## Update helm chart docs
 chart-docs:
@@ -16,3 +18,21 @@ lint-and-install-chart: ## Lints and installs helm chart
 lint-and-install-chart:
 	ct lint-and-install --config charts/ct-config.yaml
 	ct lint-and-install --config charts/ct-config.yaml --upgrade
+
+.PHONY: schema-chart
+schema-chart: ## Updates helm values JSON schema
+schema-chart: helm-values-schema-plugin
+	HELM_PLUGINS=$(HELM_PLUGINS) helm schema \
+	--input charts/cluster-api-runtime-extensions-nutanix/values.yaml \
+	--output charts/cluster-api-runtime-extensions-nutanix/values.schema.json
+
+.PHONY: helm-values-schema-plugin
+helm-values-schema-plugin: ## Installs helm-values-schema plugin
+helm-values-schema-plugin:
+	# Only install if plugin not yet installed
+	HELM_PLUGINS=$(HELM_PLUGINS) \
+	helm plugin list | grep --silent "^schema\s" \
+		|| \
+	HELM_PLUGINS=$(HELM_PLUGINS) \
+	helm plugin install \
+	https://github.com/losisin/helm-values-schema-json.git
