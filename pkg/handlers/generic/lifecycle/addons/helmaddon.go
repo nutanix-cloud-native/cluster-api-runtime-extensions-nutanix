dcd84a03259f5e9ab057f4195c910e4015304c25
Jimmi Dyson <jimmidyson@gmail.com>
2024-08-07 17:59:55 +0100

    fix: Handle long cluster names
    This commit starts fixing a bug that means
    addons fail to be fully deployed if the cluster name
    is longer than 44 characters. This is caused by the name
    of the HCP being over 63 characters. This name is then
    used in HRP labels which have a maximum length of 63
    characters, so the HRPs are rejected by the API server
    when CAAPH applies them.
    
    The fix is to use generate name and labels on the
    HCP to ensure uniqueness by lookup rather than by using
    a deterministic name.

--- pkg/handlers/generic/lifecycle/addons/helmaddon.go
+++ pkg/handlers/generic/lifecycle/addons/helmaddon.go
@@ -5,6 +5,7 @@ package addons
 
 import (
 	"context"
+	"crypto/sha256"
 	"fmt"
 
 	"github.com/go-logr/logr"
@@ -18,6 +19,11 @@ import (
 	k8sclient "github.com/nutanix-cloud-native/cluster-api-runtime-extensions-nutanix/common/pkg/k8s/client"
 	lifecycleconfig "github.com/nutanix-cloud-native/cluster-api-runtime-extensions-nutanix/pkg/handlers/generic/lifecycle/config"
 	handlersutils "github.com/nutanix-cloud-native/cluster-api-runtime-extensions-nutanix/pkg/handlers/utils"
+)
+
+var (
+	HelmReleaseNameHashLabel = "addons.cluster.x-k8s.io/helm-release-name-hash"
+	ClusterNamespaceLabel    = clusterv1.ClusterNamespaceAnnotation
 )
 
 type HelmAddonConfig struct {
@@ -52,6 +58,7 @@ type helmAddonApplier struct {
 	config    *HelmAddonConfig
 	client    ctrlclient.Client
 	helmChart *lifecycleconfig.HelmChart
+	opts      []applyOption
 }
 
 var _ Applier = &helmAddonApplier{}
@@ -68,12 +75,48 @@ func NewHelmAddonApplier(
 	}
 }
 
+type applyOptions struct {
+	valueTemplater func(cluster *clusterv1.Cluster, valuesTemplate string) (string, error)
+	targetCluster  *clusterv1.Cluster
+}
+
+type applyOption func(*applyOptions)
+
+func (a *helmAddonApplier) WithValueTemplater(
+	valueTemplater func(cluster *clusterv1.Cluster, valuesTemplate string) (string, error),
+) *helmAddonApplier {
+	a.opts = append(a.opts, func(o *applyOptions) {
+		o.valueTemplater = valueTemplater
+	})
+
+	return a
+}
+
+func (a *helmAddonApplier) WithTargetCluster(cluster *clusterv1.Cluster) *helmAddonApplier {
+	a.opts = append(a.opts, func(o *applyOptions) {
+		o.targetCluster = cluster
+	})
+
+	return a
+}
+
 func (a *helmAddonApplier) Apply(
 	ctx context.Context,
 	cluster *clusterv1.Cluster,
 	defaultsNamespace string,
 	log logr.Logger,
 ) error {
+	applyOpts := &applyOptions{}
+	for _, opt := range a.opts {
+		opt(applyOpts)
+	}
+
+	log.Info("Checking for existing HelmChartProxy for cluster")
+	chartProxy, err := a.FindExistingHelmChartProxy(ctx, cluster)
+	if err != nil {
+		return fmt.Errorf("failed to lookup existing HelmChartProxy for cluster: %w", err)
+	}
+
 	log.Info("Retrieving installation values template for cluster")
 	values, err := handlersutils.RetrieveValuesTemplate(
 		ctx,
@@ -88,29 +131,52 @@ func (a *helmAddonApplier) Apply(
 		)
 	}
 
-	chartProxy := &caaphv1.HelmChartProxy{
-		TypeMeta: metav1.TypeMeta{
-			APIVersion: caaphv1.GroupVersion.String(),
-			Kind:       "HelmChartProxy",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Namespace: cluster.Namespace,
-			Name:      a.config.defaultHelmReleaseName + "-" + cluster.Name,
-		},
-		Spec: caaphv1.HelmChartProxySpec{
-			RepoURL:   a.helmChart.Repository,
-			ChartName: a.helmChart.Name,
-			ClusterSelector: metav1.LabelSelector{
-				MatchLabels: map[string]string{clusterv1.ClusterNameLabel: cluster.Name},
-			},
-			ReleaseNamespace: a.config.defaultHelmReleaseNamespace,
-			ReleaseName:      a.config.defaultHelmReleaseName,
-			Version:          a.helmChart.Version,
-			ValuesTemplate:   values,
-		},
+	if applyOpts.valueTemplater != nil {
+		values, err = applyOpts.valueTemplater(cluster, values)
+		if err != nil {
+			return fmt.Errorf("failed to template Helm values: %w", err)
+		}
 	}
+
+	targetCluster := cluster
+	if applyOpts.targetCluster != nil {
+		targetCluster = applyOpts.targetCluster
+	}
+
+	if chartProxy == nil {
+		chartProxy = &caaphv1.HelmChartProxy{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: caaphv1.GroupVersion.String(),
+				Kind:       "HelmChartProxy",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: targetCluster.Namespace,
+				Labels: map[string]string{
+					clusterv1.ClusterNameLabel: cluster.Name,
+					ClusterNamespaceLabel:      cluster.Namespace,
+					// Label values have a maximum length of 63 characters so hash the release name to
+					// ensure it fits within the limit.
+					HelmReleaseNameHashLabel: hashReleaseName(a.config.defaultHelmReleaseName),
+				},
+				GenerateName: fmt.Sprintf("%s-", a.config.defaultHelmReleaseName),
+			},
+		}
+	}
+
+	chartProxy.Spec = caaphv1.HelmChartProxySpec{
+		RepoURL:   a.helmChart.Repository,
+		ChartName: a.helmChart.Name,
+		ClusterSelector: metav1.LabelSelector{
+			MatchLabels: map[string]string{clusterv1.ClusterNameLabel: targetCluster.Name},
+		},
+		ReleaseNamespace: a.config.defaultHelmReleaseNamespace,
+		ReleaseName:      a.config.defaultHelmReleaseName,
+		Version:          a.helmChart.Version,
+		ValuesTemplate:   values,
+	}
+
 	handlersutils.SetTLSConfigForHelmChartProxyIfNeeded(chartProxy)
-	if err = controllerutil.SetOwnerReference(cluster, chartProxy, a.client.Scheme()); err != nil {
+	if err = controllerutil.SetOwnerReference(targetCluster, chartProxy, a.client.Scheme()); err != nil {
 		return fmt.Errorf(
 			"failed to set owner reference on HelmChartProxy %q: %w",
 			chartProxy.Name,
@@ -118,9 +184,62 @@ func (a *helmAddonApplier) Apply(
 		)
 	}
 
-	if err = k8sclient.ServerSideApply(ctx, a.client, chartProxy, k8sclient.ForceOwnership); err != nil {
-		return fmt.Errorf("failed to apply HelmChartProxy %q: %w", chartProxy.Name, err)
+	// Only server-side apply the HelmChartProxy if it already existed, i.e. that metadata.name is non-empty.
+	// This allows to use metadata.generateName for the first creation to avoid naming collisions.
+	if chartProxy.Name == "" {
+		if err = a.client.Create(ctx, chartProxy); err != nil {
+			return fmt.Errorf("failed to create HelmChartProxy %q: %w", chartProxy.Name, err)
+		}
+	} else {
+		// metadata.managedFields must be nil when using server-side apply.
+		chartProxy.ManagedFields = nil
+		if err = k8sclient.ServerSideApply(ctx, a.client, chartProxy, k8sclient.ForceOwnership); err != nil {
+			return fmt.Errorf("failed to apply HelmChartProxy %q: %w", chartProxy.Name, err)
+		}
 	}
 
 	return nil
+}
+
+func (a *helmAddonApplier) FindExistingHelmChartProxy(
+	ctx context.Context, cluster *clusterv1.Cluster,
+) (*caaphv1.HelmChartProxy, error) {
+	applyOpts := &applyOptions{}
+	for _, opt := range a.opts {
+		opt(applyOpts)
+	}
+
+	targetCluster := cluster
+	if applyOpts.targetCluster != nil {
+		targetCluster = applyOpts.targetCluster
+	}
+
+	chartProxyList := &caaphv1.HelmChartProxyList{}
+	if err := a.client.List(
+		ctx,
+		chartProxyList,
+		ctrlclient.MatchingLabels{
+			clusterv1.ClusterNameLabel: cluster.Name,
+			ClusterNamespaceLabel:      cluster.Namespace,
+			HelmReleaseNameHashLabel:   hashReleaseName(a.config.defaultHelmReleaseName),
+		},
+		ctrlclient.InNamespace(targetCluster.Namespace),
+	); err != nil {
+		return nil, fmt.Errorf("failed to list HelmChartProxies: %w", err)
+	}
+
+	if len(chartProxyList.Items) == 0 {
+		return nil, nil
+	}
+
+	if len(chartProxyList.Items) > 1 {
+		return nil, fmt.Errorf("found multiple HelmChartProxies for cluster %q", cluster.Name)
+	}
+
+	return &chartProxyList.Items[0], nil
+}
+
+func hashReleaseName(releaseName string) string {
+	// Use Sum224 to ensure the hash is 56 characters long.
+	return fmt.Sprintf("%x", sha256.Sum224([]byte(releaseName)))
 }
