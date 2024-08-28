7a36e229a9e61aec392f46ff84c0c11310fe3cee
Daniel Lipovetsky <daniel.lipovetsky@nutanix.com>
2024-08-28 03:13:31 -0700

    feat: Upgrade to Calico v3.28.1 (#890)
    **What problem does this PR solve?**:
    Backport of #878. Upstream fixes include a performance regression with
    Linux kernel 5.4.x.
    
    Do not merge for now.
    
    **Which issue(s) this PR fixes**:
    Fixes #
    
    **How Has This Been Tested?**:
    <!--
    Please describe the tests that you ran to verify your changes.
    Provide output from the tests and any manual steps needed to replicate
    the tests.
    -->
    
    **Special notes for your reviewer**:
    <!--
    Use this to provide any additional information to the reviewers.
    This may include:
    - Best way to review the PR.
    - Where the author wants the most review attention on.
    - etc.
    -->

--- make/addons.mk
+++ make/addons.mk
@@ -1,7 +1,7 @@
 # Copyright 2023 Nutanix. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-export CALICO_VERSION := v3.28.0
+export CALICO_VERSION := v3.28.1
 export CILIUM_VERSION := 1.15.6
 export NODE_FEATURE_DISCOVERY_VERSION := 0.16.1
 export CLUSTER_AUTOSCALER_CHART_VERSION := 9.37.0
