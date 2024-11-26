0b01878797c64fbbbbee0c3a881619b8371513ea
Jimmi Dyson <jimmidyson@gmail.com>
2024-11-26 17:28:55 +0000

    build: Add CAREN release to cluster class artifacts (#979)
    This attaches the updated cluster class artifacts containing the
    CAREN release version to the release artifacts on release.

---
+++ hack/examples/release/add-version-to-clusterclasses.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Copyright 2024 Nutanix. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set -euo pipefail
+IFS=$'\n\t'
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+readonly SCRIPT_DIR
+
+trap 'find "${SCRIPT_DIR}" -name kustomization.yaml -delete' EXIT
+
+export CAREN_RELEASE_VERSION="${1}"
+
+for CC_TEMPLATE in "${SCRIPT_DIR}"/../../../charts/cluster-api-runtime-extensions-nutanix/defaultclusterclasses/*.yaml; do
+  export CC_TEMPLATE
+  envsubst -no-empty -no-unset -i "${SCRIPT_DIR}/kustomization.yaml.tmpl" -o "${SCRIPT_DIR}/kustomization.yaml"
+
+  kustomize build "${SCRIPT_DIR}" --load-restrictor LoadRestrictionsNone >"${SCRIPT_DIR}/$(basename "${CC_TEMPLATE}")"
+done
