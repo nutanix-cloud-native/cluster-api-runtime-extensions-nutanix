6e6c70bd8acdc94fe33de7e510070d2baee7e3fb
Dimitri Koshkin <dimitri.koshkin@nutanix.com>
2025-05-07 11:30:20 -0700

    fixup! docs: package level comment

--- pkg/handlers/generic/lifecycle/registrymirror/doc.go
+++ pkg/handlers/generic/lifecycle/registrymirror/doc.go
@@ -1,4 +1,6 @@
 // Copyright 2025 Nutanix. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+// Package registrymirror provides a handler for managing a registry addon in clusters.
+// The clusters will also be configured to use the registry as a Containerd mirror (done in a different handler).
 package registrymirror
