24c9d4553e26c4ff6a80aa05026bad1a6dca57d5
Daniel Lipovetsky <daniel.lipovetsky@gmail.com>
2025-05-20 11:27:51 -0700

    fixup! feat: Add preflight checks framework
    Extend timeout to 30s

--- pkg/webhook/preflight/doc.go
+++ pkg/webhook/preflight/doc.go
@@ -2,4 +2,4 @@
 // SPDX-License-Identifier: Apache-2.0
 package preflight
 
-// +kubebuilder:webhook:path=/preflight-v1beta1,mutating=false,failurePolicy=fail,groups="cluster.x-k8s.io",resources=clusters,verbs=create;update,versions=*,name=preflight.caren.nutanix.com,admissionReviewVersions=v1,sideEffects=None
+// +kubebuilder:webhook:path=/preflight-v1beta1,mutating=false,failurePolicy=fail,groups="cluster.x-k8s.io",resources=clusters,verbs=create;update,versions=*,name=preflight.caren.nutanix.com,admissionReviewVersions=v1,sideEffects=None,timeoutSeconds=30
