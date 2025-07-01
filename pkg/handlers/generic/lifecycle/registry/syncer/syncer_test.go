0606f403c3a8bb6320951d89ff34059e528a0d98
Dimitri Koshkin <dimitri.koshkin@nutanix.com>
2025-07-01 13:36:39 -0700

    fixup! test: embed test files

--- pkg/handlers/generic/lifecycle/registry/syncer/syncer_test.go
+++ pkg/handlers/generic/lifecycle/registry/syncer/syncer_test.go
@@ -4,6 +4,7 @@
 package syncer
 
 import (
+	_ "embed"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,6 +17,14 @@ import (
 	carenv1 "github.com/nutanix-cloud-native/cluster-api-runtime-extensions-nutanix/api/v1alpha1"
 	"github.com/nutanix-cloud-native/cluster-api-runtime-extensions-nutanix/api/variables"
 	"github.com/nutanix-cloud-native/cluster-api-runtime-extensions-nutanix/pkg/feature"
+)
+
+var (
+	//go:embed testdata/registry-syncer-template.yaml.tmpl
+	testRegistrySyncerTemplate string
+
+	//go:embed testdata/registry-syncer-values.yaml
+	expectedRegistrySyncerValues string
 )
 
 func Test_shouldApplyRegistrySyncer(t *testing.T) {
@@ -97,7 +106,7 @@ func Test_shouldApplyRegistrySyncer(t *testing.T) {
 func Test_templateValues(t *testing.T) {
 	result, err := templateValues(namedClusterWithRegistry(t, "test-cluster"), testRegistrySyncerTemplate)
 	require.NoError(t, err)
-	assert.Equal(t, expectedRegistrySyncerTemplate, result)
+	assert.Equal(t, expectedRegistrySyncerValues, result)
 }
 
 func clusterWithRegistry(t *testing.T) *clusterv1.Cluster {
@@ -135,6 +144,7 @@ func namedClusterWithRegistry(t *testing.T, name string) *clusterv1.Cluster {
 				Variables: []clusterv1.ClusterVariable{
 					*variable,
 				},
+				Version: "v1.30.100",
 			},
 		},
 	}
@@ -183,179 +193,3 @@ func enableFeatureGate(t *testing.T, value bool) {
 		value,
 	)
 }
-
-const (
-	testRegistrySyncerTemplate = `initContainers:
-  # The regsync container does not fail when it cannot connect to the destination registry.
-  # In the case when it runs as a Job, it will prematurely exit.
-  # This init container will wait for the destination registry to be ready.
-  - name: wait-for-registry
-    image: ghcr.io/d2iq-labs/kubectl-betterwait:{{ .KubernetesVersion }}
-    args:
-      - --for=condition=Ready
-      - --timeout=-1s # a negative number here means wait forever
-      - --interval=5s # poll every 5 seconds to the resources to be created
-      - --namespace={{ .DestinationRegistryHeadlessServiceNamespace }}
-      - --kubeconfig=/kubeconfig/admin.conf
-      # Ideally we would wait for the Service to be ready, but Kubernetes does not have a condition for that.
-      - pod/{{ .DestinationRegistryAnyPodName }}
-    volumeMounts:
-      - mountPath: /kubeconfig
-        name: kubeconfig
-        readOnly: true
-  - name: port-forward-registry
-    image: ghcr.io/d2iq-labs/kubectl-betterwait:{{ .KubernetesVersion }}
-    command:
-      - /bin/kubectl
-    args:
-      - port-forward
-      - --address=127.0.0.1
-      - --namespace={{ .DestinationRegistryHeadlessServiceNamespace }}
-      - --kubeconfig=/kubeconfig/admin.conf
-      # This will port-forward to a single Pod in the Service.
-      - service/{{ .DestinationRegistryHeadlessServiceName }}
-      - 5000:{{ .DestinationRegistryHeadlessServicePort }}
-    resources:
-      requests:
-        cpu: 25m
-        memory: 32Mi
-      limits:
-        cpu: 100m
-        memory: 50Mi
-    volumeMounts:
-      - mountPath: /kubeconfig
-        name: kubeconfig
-        readOnly: true
-    # Kubernetes will treat this as a Sidecar container
-    # https://kubernetes.io/docs/concepts/workloads/pods/sidecar-containers/
-    restartPolicy: Always
-
-extraVolumes:
-  - name: kubeconfig
-    secret:
-      items:
-        - key: value
-          path: admin.conf
-      secretName: {{ .CusterName }}-kubeconfig
-  - name: ca-cert
-    secret:
-      secretName: {{ .RegistryCASecretName }}
-
-extraVolumeMounts:
-  # Assume both the source and the target registries have the same CA.
-  # Source registry running in the cluster.
-  - mountPath: /etc/docker/certs.d/{{ .SourceRegistryAddress }}/
-    name: ca-cert
-    readOnly: true
-  # Destination registry running in the remote cluster being port-forwarded.
-  - mountPath: /etc/docker/certs.d/127.0.0.1:5000/
-    name: ca-cert
-    readOnly: true
-
-deployment:
-  config:
-    creds:
-      - registry: {{ .SourceRegistryAddress }}
-        reqPerSec: 1
-    sync:
-      - source: {{ .SourceRegistryAddress }}
-        target: 127.0.0.1:5000
-        type: registry
-        interval: 1m
-
-job:
-  enabled: true
-  config:
-    sync:
-      - source: {{ .SourceRegistryAddress }}
-        target: 127.0.0.1:5000
-        type: registry
-        interval: 1m`
-
-	expectedRegistrySyncerTemplate = `initContainers:
-  # The regsync container does not fail when it cannot connect to the destination registry.
-  # In the case when it runs as a Job, it will prematurely exit.
-  # This init container will wait for the destination registry to be ready.
-  - name: wait-for-registry
-    image: ghcr.io/d2iq-labs/kubectl-betterwait:
-    args:
-      - --for=condition=Ready
-      - --timeout=-1s # a negative number here means wait forever
-      - --interval=5s # poll every 5 seconds to the resources to be created
-      - --namespace=registry-system
-      - --kubeconfig=/kubeconfig/admin.conf
-      # Ideally we would wait for the Service to be ready, but Kubernetes does not have a condition for that.
-      - pod/cncf-distribution-registry-docker-registry-0
-    volumeMounts:
-      - mountPath: /kubeconfig
-        name: kubeconfig
-        readOnly: true
-  - name: port-forward-registry
-    image: ghcr.io/d2iq-labs/kubectl-betterwait:
-    command:
-      - /bin/kubectl
-    args:
-      - port-forward
-      - --address=127.0.0.1
-      - --namespace=registry-system
-      - --kubeconfig=/kubeconfig/admin.conf
-      # This will port-forward to a single Pod in the Service.
-      - service/cncf-distribution-registry-docker-registry-headless
-      - 5000:5000
-    resources:
-      requests:
-        cpu: 25m
-        memory: 32Mi
-      limits:
-        cpu: 100m
-        memory: 50Mi
-    volumeMounts:
-      - mountPath: /kubeconfig
-        name: kubeconfig
-        readOnly: true
-    # Kubernetes will treat this as a Sidecar container
-    # https://kubernetes.io/docs/concepts/workloads/pods/sidecar-containers/
-    restartPolicy: Always
-
-extraVolumes:
-  - name: kubeconfig
-    secret:
-      items:
-        - key: value
-          path: admin.conf
-      secretName: test-cluster-kubeconfig
-  - name: ca-cert
-    secret:
-      secretName: test-cluster-registry-addon-ca
-
-extraVolumeMounts:
-  # Assume both the source and the target registries have the same CA.
-  # Source registry running in the cluster.
-  - mountPath: /etc/docker/certs.d/cncf-distribution-registry-docker-registry.registry-system.svc.cluster.local:443/
-    name: ca-cert
-    readOnly: true
-  # Destination registry running in the remote cluster being port-forwarded.
-  - mountPath: /etc/docker/certs.d/127.0.0.1:5000/
-    name: ca-cert
-    readOnly: true
-
-deployment:
-  config:
-    creds:
-      - registry: cncf-distribution-registry-docker-registry.registry-system.svc.cluster.local:443
-        reqPerSec: 1
-    sync:
-      - source: cncf-distribution-registry-docker-registry.registry-system.svc.cluster.local:443
-        target: 127.0.0.1:5000
-        type: registry
-        interval: 1m
-
-job:
-  enabled: true
-  config:
-    sync:
-      - source: cncf-distribution-registry-docker-registry.registry-system.svc.cluster.local:443
-        target: 127.0.0.1:5000
-        type: registry
-        interval: 1m`
-)
