279c4bf8b938cc07ecb3db9476a36ce9e35376e7
Jimmi Dyson <jimmidyson@gmail.com>
2025-08-06 13:12:20 +0100

    feat: Pull in EKS APIs

--- make/clusterctl.mk
+++ make/clusterctl.mk
@@ -3,7 +3,8 @@
 
 export CAPI_VERSION := $(shell GOWORK=off go list -m -f '{{ .Version }}' sigs.k8s.io/cluster-api)
 export CAPD_VERSION := $(shell GOWORK=off go list -m -f '{{ .Version }}' sigs.k8s.io/cluster-api/test)
-export CAPA_VERSION := $(shell cd hack/third-party/capa && GOWORK=off go list -m -f '{{ .Version }}' sigs.k8s.io/cluster-api-provider-aws/v2)
+# export CAPA_VERSION := $(shell cd hack/third-party/capa && GOWORK=off go list -m -f '{{ .Version }}' sigs.k8s.io/cluster-api-provider-aws/v2)
+export CAPA_VERSION := v2.8.4
 export CAPX_VERSION := $(shell cd hack/third-party/capx && GOWORK=off go list -m -f '{{ .Version }}' github.com/nutanix-cloud-native/cluster-api-provider-nutanix)
 export CAAPH_VERSION := $(shell cd hack/third-party/caaph && GOWORK=off go list -m -f '{{ .Version }}' sigs.k8s.io/cluster-api-addon-provider-helm)
 
@@ -13,6 +14,7 @@ clusterctl.init:
 	env CLUSTER_TOPOLOGY=true \
 	    EXP_RUNTIME_SDK=true \
 	    EXP_MACHINE_POOL=true \
+	    CAPA_EKS=true \
 	    AWS_B64ENCODED_CREDENTIALS=$$(clusterctl-aws bootstrap credentials encode-as-profile) \
 	    NUTANIX_ENDPOINT="" NUTANIX_PASSWORD="" NUTANIX_USER="" \
 	    clusterctl init \
@@ -23,7 +25,17 @@ clusterctl.init:
 	      --infrastructure docker:$(CAPD_VERSION),aws:$(CAPA_VERSION),nutanix:$(CAPX_VERSION) \
 	      --addon helm:$(CAAPH_VERSION) \
 	      --wait-providers
+	kubectl apply --server-side --force-conflicts \
+		-k 'https://github.com/jimmidyson/cluster-api-provider-aws/config/crd?ref=$(CAPA_VERSION)-eksclusterclass'
+	kubectl set image -n capa-system deployment/capa-controller-manager \
+		manager=ghcr.io/jimmidyson/cluster-api-aws/cluster-api-aws-controller:$(CAPA_VERSION)-eksclusterclass
 
 .PHONY: clusterctl.delete
 clusterctl.delete:
 	clusterctl delete --kubeconfig=$(KIND_KUBECONFIG) --all
+
+.PHONY: capa.update-credentials-secret
+capa.update-credentials-secret:
+	kubectl patch secret capa-manager-bootstrap-credentials -n capa-system -p="{\"data\":{\"credentials\": \"$$(clusterctl-aws bootstrap credentials encode-as-profile)\"}}"
+	kubectl rollout restart deployment capa-controller-manager -n capa-system
+	kubectl rollout status deployment capa-controller-manager -n capa-system
