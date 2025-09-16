e24db2bccc6b590566059a6b8c862c5db6c4c765
Dimitri Koshkin <dimitri.koshkin@gmail.com>
2025-09-16 13:23:32 -0700

    test: new LoadBalancer Service e2e test (#1304)
    **What problem does this PR solve?**:
    Creates a new check in Nutanix e2e tests that reservces an IP in the
    Subnet for metallb, creates a Deployment and Service and verifies its
    reachable from inside the runner.
    
    **Which issue(s) this PR fixes**:
    Fixes #
    
    **How Has This Been Tested?**:
    <!--
    Please describe the tests that you ran to verify your changes.
    Provide output from the tests and any manual steps needed to replicate
    the tests.
    -->
    
    **Special notes for your reviewer**:
    <!--
    Use this to provide any additional information to the reviewers.
    This may include:
    - Best way to review the PR.
    - Where the author wants the most review attention on.
    - etc.
    -->

--- test/e2e/serviceloadbalancer_helpers.go
+++ test/e2e/serviceloadbalancer_helpers.go
@@ -8,11 +8,19 @@ package e2e
 import (
 	"context"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/utils/ptr"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 	"sigs.k8s.io/cluster-api/test/framework"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -135,4 +143,155 @@ func waitForMetalLBServiceLoadBalancerToBeReadyInWorkloadCluster(
 		Getter:    workloadClusterClient,
 		Resources: resources,
 	}, input.resourceIntervals...)
+}
+
+type EnsureLoadBalancerServiceInput struct {
+	WorkloadCluster  *clusterv1.Cluster
+	ClusterProxy     framework.ClusterProxy
+	ServiceIntervals []interface{}
+}
+
+// EnsureLoadBalancerService creates a test Service of type LoadBalancer and tests that the assigned IP responds.
+func EnsureLoadBalancerService(
+	ctx context.Context,
+	input EnsureLoadBalancerServiceInput,
+) {
+	workloadClusterClient := input.ClusterProxy.GetWorkloadCluster(
+		ctx, input.WorkloadCluster.Namespace, input.WorkloadCluster.Name,
+	).GetClient()
+
+	svc := createTestService(ctx, workloadClusterClient, input.ServiceIntervals)
+
+	By("Testing the LoadBalancer Service responds")
+	getClientIPURL := &url.URL{
+		Scheme: "http",
+		Host:   getLoadBalancerAddress(svc),
+		Path:   "/clientip",
+	}
+	output := testServiceLoadBalancer(ctx, getClientIPURL, input.ServiceIntervals)
+	Expect(output).ToNot(BeEmpty())
+}
+
+func createTestService(
+	ctx context.Context,
+	workloadClusterClient client.Client,
+	intervals []interface{},
+) *corev1.Service {
+	const (
+		name      = "echo"
+		namespace = corev1.NamespaceDefault
+		appKey    = "app"
+		replicas  = int32(1)
+		image     = "registry.k8s.io/e2e-test-images/agnhost:2.57"
+		port      = 8080
+		portName  = "http"
+	)
+
+	By("Creating a test Deployment for LoadBalancer Service")
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: ptr.To(replicas),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{appKey: name},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{appKey: name},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{{
+						Name:  name,
+						Image: image,
+						Args:  []string{"netexec", fmt.Sprintf("--http-port=%d", port)},
+						Ports: []corev1.ContainerPort{{
+							Name:          portName,
+							ContainerPort: int32(port),
+						}},
+					}},
+				},
+			},
+		},
+	}
+	if err := workloadClusterClient.Create(ctx, deployment); err != nil {
+		Expect(err).ToNot(HaveOccurred())
+	}
+	By("Waiting for Deployment to be ready")
+	Eventually(func(g Gomega) {
+		g.Expect(workloadClusterClient.Get(ctx, client.ObjectKeyFromObject(deployment), deployment)).To(Succeed())
+		g.Expect(deployment.Status.ReadyReplicas).To(Equal(replicas))
+	}, intervals...).Should(Succeed(), "timed out waiting for Deployment to be ready")
+
+	By("Creating a test Service for LoadBalancer Service")
+	service := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Type:     corev1.ServiceTypeLoadBalancer,
+			Selector: map[string]string{appKey: name},
+			Ports: []corev1.ServicePort{{
+				Name:       portName,
+				Port:       80,
+				Protocol:   corev1.ProtocolTCP,
+				TargetPort: intstr.FromInt(port),
+			}},
+		},
+	}
+	if err := workloadClusterClient.Create(ctx, service); err != nil {
+		Expect(err).ToNot(HaveOccurred())
+	}
+	By("Waiting for LoadBalacer IP/Hostname to be assigned")
+	Eventually(func(g Gomega) {
+		g.Expect(workloadClusterClient.Get(ctx, client.ObjectKeyFromObject(service), service)).To(Succeed())
+
+		ingress := service.Status.LoadBalancer.Ingress
+		g.Expect(ingress).ToNot(BeEmpty(), "no LoadBalancer ingress yet")
+
+		ip := ingress[0].IP
+		hostname := ingress[0].Hostname
+		g.Expect(ip == "" && hostname == "").To(BeFalse(), "ingress has neither IP nor Hostname yet")
+	}, intervals...).Should(Succeed(), "timed out waiting for LoadBalancer IP/hostname")
+
+	return service
+}
+
+func getLoadBalancerAddress(svc *corev1.Service) string {
+	ings := svc.Status.LoadBalancer.Ingress
+	if len(ings) == 0 {
+		return ""
+	}
+	address := ings[0].IP
+	if address == "" {
+		address = ings[0].Hostname
+	}
+	return address
+}
+
+func testServiceLoadBalancer(
+	ctx context.Context,
+	requestURL *url.URL,
+	intervals []interface{},
+) string {
+	hc := &http.Client{Timeout: 5 * time.Second}
+	var output string
+	Eventually(func(g Gomega) string {
+		req, _ := http.NewRequestWithContext(ctx, http.MethodGet, requestURL.String(), http.NoBody)
+		resp, err := hc.Do(req)
+		if err != nil {
+			return ""
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != http.StatusOK {
+			return ""
+		}
+		b, _ := io.ReadAll(resp.Body)
+		output = strings.TrimSpace(string(b))
+		return output
+	}, intervals...).ShouldNot(BeEmpty(), "no response from service")
+	return output
 }
