98aeca6fb39c14ff0162fe74a7b9af4edd7e03dd
Priyadarshini Kumari <priyadarshini.kumari@nutanix.com>
2026-04-09 17:57:16 +0530

    fix(caren): fix tests

--- pkg/handlers/lifecycle/konnectoragent/handler.go
+++ pkg/handlers/lifecycle/konnectoragent/handler.go
@@ -196,9 +196,10 @@ func (n *DefaultKonnectorAgent) apply(
 		return
 	}
 
-	prismCredentialsSecretName := defaultCredentialsSecretName
-	if k8sAgentVar.PrismCredentialsSecretName != "" {
-		prismCredentialsSecretName = k8sAgentVar.PrismCredentialsSecretName
+	// Use the provided source Secret name so we don't require hardcoded names in the values template.
+	prismCredentialsSecretName := k8sAgentVar.Credentials.SecretRef.Name
+	if prismCredentialsSecretName == "" {
+		prismCredentialsSecretName = defaultCredentialsSecretName
 	}
 
 	// It's possible to have the credentials Secret be created by the Helm chart.
