8476a6dd10c36e608fcc44968c2ae18dabc51a92
vijayaraghavanr31 <vijayaraghavan.r@nutanix.com>
2026-09-12 14:43:23 +0530

    fix: bump up konnector to 1.4.2 (#1713)
    Bump up konnector agent to 1.4.2 release tag.

--- make/addons.mk
+++ make/addons.mk
@@ -139,11 +139,11 @@ export COSI_CONTROLLER_VERSION := 0.2.2
 # Konnector Agent
 #   Chart name:    konnector-agent
 #   Chart repo: 	 https://nutanix.github.io/helm-releases/index.yaml
-#   Chart version: 1.4.1
-#   App version:   v1.4.1
+#   Chart version: 1.4.2
+#   App version:   v1.4.2
 #   Repo:          https://github.com/nutanix-core/k8s-agent
-#   Release:       https://github.com/nutanix-core/k8s-agent/releases/tag/1.4.0
-export KONNECTOR_AGENT_VERSION := 1.4.1
+#   Release:       https://github.com/nutanix-core/k8s-agent/releases/tag/1.4.2
+export KONNECTOR_AGENT_VERSION := 1.4.2
 
 # Multus
 #   Chart name: multus
